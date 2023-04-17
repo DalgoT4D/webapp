810cabe8ee2ca5d463c6fad11163a1541e5ad48b
mdshamoon <mohdshamoon841@gmail.com>
2023-04-17 15:20:18 +0530

    added ingest page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/config/theme';
 import { Box } from '@mui/material';
 import { SideDrawer } from '@/components/SideDrawer/SideDrawer';
+import { SWRConfig } from 'swr';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,7 +15,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <Box sx={{ display: 'flex' }}>
           <SideDrawer />
           <CssBaseline />
-          <Component {...pageProps} />
+          <SWRConfig
+            value={{
+              refreshInterval: 3000,
+              fetcher: (resource, init) =>
+                fetch(resource, {
+                  headers: {
+                    Authorization:
+                      'Bearer 6f72efcd4b4e2bdc9a20703b07bc912c92172414',
+                  },
+                }).then((res) => res.json()),
+            }}
+          >
+            <Component {...pageProps} />
+          </SWRConfig>
         </Box>
       </ThemeProvider>
     </main>
