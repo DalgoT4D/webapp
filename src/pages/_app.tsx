97c4d63cf3e5503835653a035e750b1b503c6aff
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-04-17 15:00:07 +0530

    add layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,20 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/config/theme';
 import { Box } from '@mui/material';
+
 import { SideDrawer } from '@/components/SideDrawer/SideDrawer';
+import { Main } from '@/components/Layouts/Main';
+import { Auth } from '@/components/Layouts/Auth';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={rajdhani.className}>
       <ThemeProvider theme={theme}>
         <Box sx={{ display: 'flex' }}>
-          <SideDrawer />
           <CssBaseline />
-          <Component {...pageProps} />
+          <Main>
+            <Component {...pageProps} />
+          </Main>
         </Box>
       </ThemeProvider>
     </main>
