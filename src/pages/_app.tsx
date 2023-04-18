e2981517f3340c62e35b8dced6640d67f2fc8399
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-04-18 14:22:47 +0530

    add auth guards and login screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,31 +6,32 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/config/theme';
 import { Box } from '@mui/material';
 import { SWRConfig } from 'swr';
+import { SessionProvider } from 'next-auth/react';
 
 import { Main } from '@/components/Layouts/Main';
-import { Auth } from '@/components/Layouts/Auth';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <main className={rajdhani.className}>
       <ThemeProvider theme={theme}>
         <Box sx={{ display: 'flex' }}>
           <CssBaseline />
-          <SWRConfig
-            value={{
-              refreshInterval: 3000,
-              fetcher: (resource, init) =>
-                fetch(resource, {
-                  headers: {
-                    Authorization: 'Bearer ',
-                  },
-                }).then((res) => res.json()),
-            }}
-          >
+          <SessionProvider session={session}>
             <Main>
-              <Component {...pageProps} />
+              <SWRConfig
+                value={{
+                  refreshInterval: 3000,
+                  fetcher: (resource, init) =>
+                    fetch(resource, init).then((res) => res.json()),
+                }}
+              >
+                <Component {...pageProps} />
+              </SWRConfig>
             </Main>
-          </SWRConfig>
+          </SessionProvider>
         </Box>
       </ThemeProvider>
     </main>
