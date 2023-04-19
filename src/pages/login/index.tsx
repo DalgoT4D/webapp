8fef5cd53c9c076369a6922cbd8cd0e767dd72e3
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-04-19 08:58:59 +0530

    login fixes

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,9 @@
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Grid, TextField } from '@mui/material';
 import { signIn } from 'next-auth/react';
 import { redirect } from 'next/dist/server/api-utils';
 import { useForm } from 'react-hook-form';
+
+import styles from '@/styles/Login.module.css';
 
 export const Login = () => {
   const { register, handleSubmit } = useForm();
@@ -15,31 +17,38 @@ export const Login = () => {
   };
 
   return (
-    <Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Box>
-          <TextField
-            id="outlined-basic"
-            label="Business email*"
-            variant="outlined"
-            {...register('username', { required: true })}
-          />
-        </Box>
-        <Box>
-          <TextField
-            id="outlined-password-input"
-            label="Password*"
-            type="password"
-            autoComplete="current-password"
-            {...register('password', { required: true })}
-          />
-        </Box>
-        <Box>
-          <Button variant="contained" type="submit">
-            Login
-          </Button>
-        </Box>
-      </form>
+    <Box className={styles.Container}>
+      <Grid container spacing={2} columns={16}>
+        <Grid item xs={8}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box className={styles.Input}>
+              <TextField
+                id="outlined-basic"
+                label="Business email"
+                variant="outlined"
+                {...register('username', { required: true })}
+              />
+            </Box>
+            <Box className={styles.Input}>
+              <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                {...register('password', { required: true })}
+              />
+            </Box>
+            <Box className={styles.Input}>
+              <Button variant="contained" type="submit">
+                Login
+              </Button>
+            </Box>
+          </form>
+        </Grid>
+        <Grid item xs={8}>
+          Banner Image
+        </Grid>
+      </Grid>
     </Box>
   );
 };
