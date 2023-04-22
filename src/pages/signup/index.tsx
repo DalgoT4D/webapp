da3d26c9c490e3aaf7576a5b5ef703bab3660e0b
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-04-22 08:42:41 +0530

    signup screen link to signup from login and vice-versa

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,20 +1,64 @@
 import { Box, Button, Grid, Paper, TextField, Link } from '@mui/material';
-import { signIn } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import Banner from '@/images/banner.png';
-
+import { backendUrl } from '@/config/constant';
+import { signIn } from 'next-auth/react';
 import styles from '@/styles/Login.module.css';
 
-export const Login = () => {
+export const SignUp = () => {
+
+  const { data: session }: any = useSession();
+  const router = useRouter();
+  if (session?.user.token) {
+    router.push('/');
+  }
+  var signupError;
   const { register, handleSubmit } = useForm();
   const onSubmit = async (data: any) => {
-    const result = await signIn('credentials', {
-      username: data.username,
-      password: data.password,
-      redirect: true,
-      callbackUrl: '/',
+    console.log("register using " + data.username + " and " + data.password);
+    signupError = null;
+    fetch(`${backendUrl}/api/organizations/users/`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${session?.user.token}`,
+      },
+      body: JSON.stringify({
+        email: data.username,
+        password: data.password,
+      }),
+    }).then((response) => {
+      console.log(response);
+      if (response.ok) {
+        console.log("success");
+        response.json().then((message) => {
+          console.log("email=" + message.email);
+          console.log("active=" + message.active);
+          console.log("role=" + message.role);
+          console.log("org=" + message.org);
+          signIn('credentials', {
+            username: data.username,
+            password: data.password,
+            redirect: true,
+            callbackUrl: '/',
+          });
+        })
+      } else {
+        console.error("caught application error");
+        response.json().then((errorMessage) => {
+          signupError = errorMessage.error;
+          console.log("set signupError = ");
+          console.log(signupError);
+        })
+      }
+    }).catch((error) => {
+      console.error("caught network error ");
+      console.error(error);
+      signupError = error;
     });
+
   };
 
   return (
@@ -49,15 +93,16 @@ export const Login = () => {
                 </Box>
                 <Box className={styles.Input}>
                   <Button variant="contained" type="submit">
-                    Login
+                    Sign Up
                   </Button>
-                  <Link href="/signup">
+                  <Link href="/login">
                     <Button variant="contained" type="button">
-                      Sign Up
+                      Login
                     </Button>
                   </Link>
                 </Box>
               </form>
+              <div>signupError: {signupError}</div>
             </Paper>
           </Grid>
         </Grid>
@@ -70,4 +115,4 @@ export const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
