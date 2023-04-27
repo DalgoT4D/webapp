390d9cc2778a7a0406475e8d0d73966a23698f6f
Ishan <koradiaishan335@gmail.com>
2023-04-27 17:22:53 +0530

    Create airbyte source - dynamic form

---
+++ src/components/MultiTagInput.tsx
@@ -0,0 +1,84 @@
+import { Box, Chip, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Delete, Close } from '@mui/icons-material';
+
+export interface MultiTagInput {
+  field: string;
+  label: string;
+  fieldValueArr: Array<string>;
+  onFieldChange: Function;
+  setFormValue: Function;
+}
+
+const MultiTagInput = ({
+  field,
+  label,
+  fieldValueArr,
+  onFieldChange,
+  setFormValue,
+}: MultiTagInput) => {
+  const [currentValue, setCurrentValue] = useState<string>('');
+
+  const handleChipClose = (e: any) => {
+    let tempArr: any = fieldValueArr || [];
+    let updatedTempArr: Array<string> = [].concat(
+      tempArr.slice(0, Number(e.target.id)),
+      tempArr.slice(Number(e.target.id) + 1, tempArr.length)
+    );
+    setFormValue(field, updatedTempArr);
+  };
+
+  const handleKeyDown = (e: any) => {
+    let tempArr = fieldValueArr || [];
+
+    switch (e.key) {
+      case 'Enter':
+        tempArr.push(currentValue);
+        setFormValue(field, tempArr);
+        setCurrentValue('');
+        break;
+      case 'Backspace':
+        if (currentValue.length === 0) {
+          tempArr = tempArr.slice(0, tempArr.length - 1);
+          setFormValue(field, tempArr);
+        }
+        break;
+    }
+  };
+
+  return (
+    <Box>
+      <TextField
+        multiline
+        variant="outlined"
+        id={'0'}
+        defaultValue={''}
+        name={field}
+        placeholder={label}
+        sx={{ width: '100%' }}
+        value={currentValue}
+        onChange={(e) => setCurrentValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+        InputProps={{
+          startAdornment: (
+            <Stack direction="row" gap="5px" flexWrap="wrap">
+              {fieldValueArr &&
+                fieldValueArr.map((ele, idx) => (
+                  <Chip
+                    id={String(idx)}
+                    key={idx}
+                    label={ele}
+                    deleteIcon={<Close />}
+                    onDelete={handleChipClose}
+                    variant="outlined"
+                  />
+                ))}
+            </Stack>
+          ),
+        }}
+      />
+    </Box>
+  );
+};
+
+export default MultiTagInput;
