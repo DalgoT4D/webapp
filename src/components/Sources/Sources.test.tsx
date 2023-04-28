94d05e9460ff7ac03efd15814eb6e79926537efd
Ishan <koradiaishan335@gmail.com>
2023-04-28 17:43:14 +0530

    unit test cases for create source and source listing

---
+++ src/components/Sources/Sources.test.tsx
@@ -0,0 +1,94 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { SessionProvider } from 'next-auth/react';
+import { Sources } from './Sources';
+import { Session } from 'next-auth';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+const user = userEvent.setup();
+
+describe('Sources', () => {
+  const mockSession: Session = {
+    expires: '1',
+    user: { email: 'a', name: 'Delta', image: 'c' },
+  };
+
+  // Mock fetch to handle useEffect in components
+  let originFetch: any;
+  beforeEach(() => {
+    const fakeResponse = {};
+    const mRes = { json: jest.fn().mockResolvedValueOnce(fakeResponse) };
+    const mockedFetch = jest.fn().mockResolvedValueOnce(mRes as any);
+    (global as any).fetch = mockedFetch;
+  });
+  afterEach(() => {
+    const fakeResponse = {};
+    const mRes = { json: jest.fn().mockResolvedValueOnce(fakeResponse) };
+    const mockedFetch = jest.fn().mockResolvedValueOnce(mRes as any);
+    (global as any).fetch = mockedFetch;
+  });
+
+  // Tests
+  it('sources list no of columns', async () => {
+    render(
+      <SessionProvider session={mockSession}>
+        <Sources />
+      </SessionProvider>
+    );
+    const columns = screen.getAllByRole('columnheader');
+    expect(columns.length).toBe(3);
+  });
+
+  it('new source button renders', async () => {
+    render(
+      <SessionProvider session={mockSession}>
+        <Sources />
+      </SessionProvider>
+    );
+    const button = screen.getByTestId('add-new-source');
+    expect(button).toHaveTextContent('+ New Source');
+  });
+
+  it('new source button click opens dialogue & close button closes it', async () => {
+    render(
+      <SessionProvider session={mockSession}>
+        <Sources />
+      </SessionProvider>
+    );
+
+    // Open dialogue box
+    const button = screen.getByTestId('add-new-source');
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(screen.getByText('Add a new source')).toBeInTheDocument();
+    });
+
+    // Close dialogue box
+    let closeButton = screen.getByTestId('CloseIcon');
+    userEvent.click(closeButton);
+    await waitFor(() => {
+      expect(() => screen.getByText('Add a new source')).toThrow();
+    });
+  });
+
+  it('new source button click opens dialogue & cancel button closes it', async () => {
+    render(
+      <SessionProvider session={mockSession}>
+        <Sources />
+      </SessionProvider>
+    );
+
+    // Open dialogue box
+    const button = screen.getByTestId('add-new-source');
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(screen.getByText('Add a new source')).toBeInTheDocument();
+    });
+
+    // Close dialogue box
+    let closeButton = screen.getByTestId('cancel');
+    userEvent.click(closeButton);
+    await waitFor(() => {
+      expect(() => screen.getByText('Add a new source')).toThrow();
+    });
+  });
+});
