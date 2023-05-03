35aea32291dc60dad1a80c02f67517007bf1755f
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-05-03 14:49:50 +0400

    more obvious styling

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -85,9 +85,7 @@ export const SignUp = () => {
                     Sign Up
                   </Button>
                   <Link href="/login">
-                    <Button variant="contained" type="button">
-                      Login
-                    </Button>
+                    Login
                   </Link>
                 </Box>
               </form>
