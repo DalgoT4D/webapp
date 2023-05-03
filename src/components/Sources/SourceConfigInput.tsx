262abd8f9a5402fd902dd7a52dbb5dc3a27b4ca8
Ishan <koradiaishan335@gmail.com>
2023-05-03 17:31:30 +0530

    handling airbyte secret fields by using password type to toggle visibility

--- src/components/Sources/SourceConfigInput.tsx
+++ src/components/Sources/SourceConfigInput.tsx
@@ -1,7 +1,9 @@
-import { Box, TextField } from '@mui/material';
-import React from 'react';
+import { Box, IconButton, InputAdornment, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import MultiTagInput from '../MultiTagInput';
 import { Controller } from 'react-hook-form';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 
 export interface SourceConfigInputprops {
   specs: Array<any>;
@@ -16,24 +18,78 @@ export const SourceConfigInput = ({
   control,
   setFormValue,
 }: SourceConfigInputprops) => {
+  const [showPasswords, setShowPasswords] = useState<any>({});
+
+  const handleClickShowPassword = (field: string) => {
+    let tempShowPasswords: any = { ...showPasswords };
+    tempShowPasswords[field] = !showPasswords[field];
+    setShowPasswords(tempShowPasswords);
+  };
+
+  useEffect(() => {
+    let tempShowPasswords: any = {};
+    specs.forEach((element) => {
+      if (element?.airbyte_secret) {
+        tempShowPasswords[element.field] = false;
+      }
+    });
+    setShowPasswords(tempShowPasswords);
+  }, [specs]);
+
   return (
     <>
       {specs
         ?.sort((input1, input2) => input1.order - input2.order)
         .map((spec: any, idx: number) =>
           spec.type === 'string' ? (
-            <React.Fragment key={idx}>
-              <TextField
-                sx={{ width: '100%' }}
-                label={spec?.title}
-                variant="outlined"
-                {...registerFormFieldValue(`config.${spec.field}`, {
-                  required: spec.required,
-                })}
-                defaultValue={spec?.default}
-              ></TextField>
-              <Box sx={{ m: 2 }} />
-            </React.Fragment>
+            spec.airbyte_secret ? (
+              <React.Fragment key={idx}>
+                <TextField
+                  sx={{ width: '100%' }}
+                  label={spec?.title}
+                  variant="outlined"
+                  type={showPasswords[`${spec.field}`] ? 'text' : 'password'}
+                  {...registerFormFieldValue(`config.${spec.field}`, {
+                    required: spec.required,
+                  })}
+                  defaultValue={spec?.default}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Box>
+                          <IconButton
+                            onClick={() =>
+                              handleClickShowPassword(`${spec.field}`)
+                            }
+                            edge="end"
+                          >
+                            {showPasswords[`${spec.field}`] ? (
+                              <VisibilityOutlinedIcon />
+                            ) : (
+                              <VisibilityOffOutlinedIcon />
+                            )}
+                          </IconButton>
+                        </Box>
+                      </InputAdornment>
+                    ),
+                  }}
+                ></TextField>
+                <Box sx={{ m: 2 }} />
+              </React.Fragment>
+            ) : (
+              <React.Fragment key={idx}>
+                <TextField
+                  sx={{ width: '100%' }}
+                  label={spec?.title}
+                  variant="outlined"
+                  {...registerFormFieldValue(`config.${spec.field}`, {
+                    required: spec.required,
+                  })}
+                  defaultValue={spec?.default}
+                ></TextField>
+                <Box sx={{ m: 2 }} />
+              </React.Fragment>
+            )
           ) : spec.type === 'array' ? (
             <React.Fragment key={idx}>
               <Controller
