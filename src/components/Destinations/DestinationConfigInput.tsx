49045a7300a3e061ae3fff13fced8386635089a7
Ishan <koradiaishan335@gmail.com>
2023-05-03 16:29:49 +0530

    input fields should be displayed in order defined in specs

--- src/components/Destinations/DestinationConfigInput.tsx
+++ src/components/Destinations/DestinationConfigInput.tsx
@@ -18,57 +18,59 @@ export const DestinationConfigInput = ({
 }: DestinationConfigInputprops) => {
   return (
     <>
-      {specs?.map((spec: any, idx: number) =>
-        spec.type === 'string' ? (
-          <React.Fragment key={idx}>
-            <TextField
-              sx={{ width: '100%' }}
-              label={spec?.title}
-              variant="outlined"
-              {...registerFormFieldValue(`config.${spec.field}`, {
-                required: spec.required,
-              })}
-              defaultValue={spec?.default}
-            ></TextField>
-            <Box sx={{ m: 2 }} />
-          </React.Fragment>
-        ) : spec.type === 'array' ? (
-          <React.Fragment key={idx}>
-            <Controller
-              name={`config.${spec.field}`}
-              control={control}
-              rules={{ required: spec.required }}
-              render={({ field: { value, onChange } }) => (
-                <MultiTagInput
-                  field={`config.${spec.field}`}
-                  label={spec.title}
-                  fieldValueArr={value}
-                  onFieldChange={onChange}
-                  setFormValue={setFormValue}
-                />
-              )}
-            />
-            <Box sx={{ m: 2 }} />
-          </React.Fragment>
-        ) : spec.type === 'integer' ? (
-          <React.Fragment key={idx}>
-            <TextField
-              sx={{ width: '100%' }}
-              label={spec?.title}
-              variant="outlined"
-              {...registerFormFieldValue(`config.${spec.field}`, {
-                required: spec.required,
-                valueAsNumber: true,
-              })}
-              defaultValue={spec?.default}
-              type="number"
-            ></TextField>
-            <Box sx={{ m: 2 }} />
-          </React.Fragment>
-        ) : (
-          ''
-        )
-      )}
+      {specs
+        .sort((input1, input2) => input1.order - input2.order)
+        ?.map((spec: any, idx: number) =>
+          spec.type === 'string' ? (
+            <React.Fragment key={idx}>
+              <TextField
+                sx={{ width: '100%' }}
+                label={spec?.title}
+                variant="outlined"
+                {...registerFormFieldValue(`config.${spec.field}`, {
+                  required: spec.required,
+                })}
+                defaultValue={spec?.default}
+              ></TextField>
+              <Box sx={{ m: 2 }} />
+            </React.Fragment>
+          ) : spec.type === 'array' ? (
+            <React.Fragment key={idx}>
+              <Controller
+                name={`config.${spec.field}`}
+                control={control}
+                rules={{ required: spec.required }}
+                render={({ field: { value, onChange } }) => (
+                  <MultiTagInput
+                    field={`config.${spec.field}`}
+                    label={spec.title}
+                    fieldValueArr={value}
+                    onFieldChange={onChange}
+                    setFormValue={setFormValue}
+                  />
+                )}
+              />
+              <Box sx={{ m: 2 }} />
+            </React.Fragment>
+          ) : spec.type === 'integer' ? (
+            <React.Fragment key={idx}>
+              <TextField
+                sx={{ width: '100%' }}
+                label={spec?.title}
+                variant="outlined"
+                {...registerFormFieldValue(`config.${spec.field}`, {
+                  required: spec.required,
+                  valueAsNumber: true,
+                })}
+                defaultValue={spec?.default}
+                type="number"
+              ></TextField>
+              <Box sx={{ m: 2 }} />
+            </React.Fragment>
+          ) : (
+            ''
+          )
+        )}
     </>
   );
 };
