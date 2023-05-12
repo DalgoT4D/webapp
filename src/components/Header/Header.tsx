e69a09172735fb3ee9276abb89a053bd4c6c358b
Ishan <koradiaishan335@gmail.com>
2023-05-12 20:13:13 +0530

    updated client side signout - clear session data

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import { Box, Button, Paper } from '@mui/material';
 import styles from './Header.module.css';
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export const Header = () => {
+  const router = useRouter();
+  const handleSignout = () => {
+    // Hit backend api to invalidate the token
+    router.push('/login');
+    signOut({ redirect: false });
+  };
   return (
     <Paper className={styles.Header}>
       <Box display="flex" justifyContent="flex-end">
         <Button
           variant="contained"
           color="secondary"
-          onClick={() => signOut()}
+          onClick={handleSignout}
           sx={{ m: 1 }}
         >
           Sign out
