86c703ccedad9e2b8671b143a2473e2a8286d226
Ishan <koradiaishan335@gmail.com>
2023-05-14 19:29:20 +0530

    minor glitches fixed

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,11 +6,11 @@ import Banner from '@/images/banner.png';
 
 import styles from '@/styles/Login.module.css';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 export const Login = () => {
   const { register, handleSubmit } = useForm();
   const router = useRouter();
+  const { data: session }: any = useSession();
 
   const onSubmit = async (reqData: any) => {
     const res: any = await signIn('credentials', {
@@ -27,51 +27,53 @@ export const Login = () => {
   };
 
   return (
-    <Box className={styles.Container}>
-      <Grid container columns={16}>
-        <Grid item xs={8}>
-          <Grid
-            container
-            height="100vh"
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Paper elevation={3} sx={{ p: 4 }}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Box className={styles.Input}>
-                  <TextField
-                    id="outlined-basic"
-                    label="Business email"
-                    variant="outlined"
-                    {...register('username', { required: true })}
-                  />
-                </Box>
-                <Box className={styles.Input}>
-                  <TextField
-                    id="outlined-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    {...register('password', { required: true })}
-                  />
-                </Box>
-                <Box className={styles.Input}>
-                  <Button variant="contained" type="submit">
-                    Login
-                  </Button>
-                  <Link href="/signup">Sign Up</Link>
-                </Box>
-              </form>
-            </Paper>
+    !session?.user?.token && (
+      <Box className={styles.Container}>
+        <Grid container columns={16}>
+          <Grid item xs={8}>
+            <Grid
+              container
+              height="100vh"
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Paper elevation={3} sx={{ p: 4 }}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Box className={styles.Input}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Business email"
+                      variant="outlined"
+                      {...register('username', { required: true })}
+                    />
+                  </Box>
+                  <Box className={styles.Input}>
+                    <TextField
+                      id="outlined-password-input"
+                      label="Password"
+                      type="password"
+                      autoComplete="current-password"
+                      {...register('password', { required: true })}
+                    />
+                  </Box>
+                  <Box className={styles.Input}>
+                    <Button variant="contained" type="submit">
+                      Login
+                    </Button>
+                    <Link href="/signup">Sign Up</Link>
+                  </Box>
+                </form>
+              </Paper>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={8}>
+            <Image src={Banner} alt="Banner" style={{ width: '100%' }} />
           </Grid>
         </Grid>
-
-        <Grid item xs={8}>
-          <Image src={Banner} alt="Banner" style={{ width: '100%' }} />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    )
   );
 };
 
