57dacd2bbd02909875f63e8fdd24269dcdacca1c
Ishan <koradiaishan335@gmail.com>
2023-05-16 18:27:55 +0530

    data flow create and index page UI setup - WIP

---
+++ src/components/Flows/Flows.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
+
+interface FlowInterface {
+  name: string;
+  schedule: string;
+  status: string;
+}
+
+interface FlowsInterface {
+  flows: Array<FlowInterface>;
+  updateCrudVal: (...args: any) => any;
+}
+
+const Flows = ({ flows, updateCrudVal }: FlowsInterface) => {
+  const handleClickCreateFlow = () => {
+    updateCrudVal('create');
+  };
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          sx={{ fontWeight: 700 }}
+          variant="h4"
+          gutterBottom
+          color="#000"
+        >
+          Flows
+        </Typography>
+        <Button
+          variant="contained"
+          onClick={handleClickCreateFlow}
+          sx={{ m: 1 }}
+        >
+          + New Flow
+        </Button>
+      </Box>
+      <Box sx={{ marginTop: '50px' }}>
+        {flows.map((flow: FlowInterface, idx: number) => (
+          <Box
+            sx={{
+              backgroundColor: 'white',
+              padding: '15px',
+              marginTop: '12px',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              borderRadius: '8px',
+            }}
+            key={idx}
+          >
+            <Box>{flow.name}</Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '30px' }}>
+              <Typography component="p" sx={{ color: '#399D47' }}>
+                Running
+              </Typography>
+              <Typography component="p">Last run 7 hrs ago</Typography>
+              <Button
+                variant="contained"
+                sx={{
+                  m: 1,
+                  backgroundColor: 'background.default',
+                  color: 'secondary.main',
+                  ':hover': { backgroundColor: 'background.default' },
+                }}
+              >
+                last log
+              </Button>
+              <Button variant="contained" sx={{ m: 1 }}>
+                Run
+              </Button>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
+};
+
+export default Flows;
--- src/pages/pipeline/orchestrate.tsx
+++ src/pages/pipeline/orchestrate.tsx
@@ -1,15 +1,39 @@
 import styles from '@/styles/Home.module.css';
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { PageHead } from '@/components/PageHead';
+import Flows from '@/components/Flows/Flows';
+import { useState } from 'react';
+import FlowCreate from '@/components/Flows/FlowCreate';
 
 export default function Orchestrate() {
+  const [crudVal, setCrudVal] = useState<string>('index'); // can be index or create
+
+  const updateCrudVal = (crudState: string) => {
+    setCrudVal(crudState);
+  };
+
+  const fakeData = [
+    {
+      name: 'flow1',
+      schedule: 'daily',
+      status: 'active',
+    },
+    {
+      name: 'flow2',
+      schedule: 'monthly',
+      status: 'active',
+    },
+  ];
+
   return (
     <>
-      <PageHead title="Development Data Platform" />
+      <PageHead title="Orchestrate" />
       <main className={styles.main}>
-        <Typography variant="h1" gutterBottom color="primary.main">
-          DDP platform orchestrate page
-        </Typography>
+        {crudVal === 'index' ? (
+          <Flows flows={fakeData} updateCrudVal={updateCrudVal} />
+        ) : (
+          <FlowCreate updateCrudVal={updateCrudVal} />
+        )}
       </main>
     </>
   );
