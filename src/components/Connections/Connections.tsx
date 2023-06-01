aa931d0ee4576a0d65cf5ceca05adbbca0d360b0
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-06-01 19:31:45 +0530

    removed celery whatnot

--- src/components/Connections/Connections.tsx
+++ src/components/Connections/Connections.tsx
@@ -5,7 +5,7 @@ import { List } from '../List/List';
 import Button from '@mui/material/Button';
 import { backendUrl } from '@/config/constant';
 import { useSession } from 'next-auth/react';
-import { httpPost } from '@/helpers/http';
+import { httpPost, httpGet } from '@/helpers/http';
 import { GlobalContext } from '@/contexts/ContextProvider';
 import { useContext } from 'react';
 import {
@@ -18,7 +18,6 @@ const headers = ['Connection details', 'Source → Destination', 'Last sync'];
 
 export const Connections = () => {
   const { data: session }: any = useSession();
-  const globalContext = useContext(GlobalContext);
 
   const [showDialog, setShowDialog] = useState(false);
   const [rows, setRows] = useState<Array<Array<string>>>([]);
@@ -26,12 +25,8 @@ export const Connections = () => {
   const { data, isLoading, mutate } = useSWR(
     `${backendUrl}/api/airbyte/connections`
   );
-  const { data: sourcesData } = useSWR(`${backendUrl}/api/airbyte/sources`);
 
   const toastContext = useContext(GlobalContext);
-
-  const [syncStatus, setSyncStatus] = useState<string>("");
-  const [syncLogs, setSyncLogs] = useState<Array<string>>([]);
 
   // when the connection list changes
   useEffect(() => {
@@ -73,10 +68,7 @@ export const Connections = () => {
           {}
         );
         if (message.success) {
-          successToast("sync started", [], toastContext);
-          if (message.celery_task_id) {
-            checkCeleryTask(message.celery_task_id);
-          }
+          successToast("Sync started... check for logs in two minutes", [], toastContext);
         }
       } catch (err: any) {
         console.error(err);
@@ -84,40 +76,6 @@ export const Connections = () => {
       }
     })();
   };
-
-  const checkCeleryTask = async (celeryTaskId: string) => {
-    try {
-      const result = await httpGet(session, `tasks/${celeryTaskId}`);
-      if (result.progress && result.progress.length > 1) {
-        const lastStep = result.progress[1]
-        if (lastStep.airbyte_job_num) {
-          const airbyteJob = await httpGet(session, `airbyte/jobs/${lastStep.airbyte_job_num}`);
-          setSyncStatus(airbyteJob.status);
-          if (airbyteJob.status === 'failed') {
-            setSyncLogs(airbyteJob.logs);
-          }
-          if (['succeeded', 'failed'].indexOf(airbyteJob.status) < 0) {
-            setTimeout(() => {
-              checkCeleryTask(celeryTaskId);
-            }, 3000);
-          }
-          // airbyteJob = {status, logs}
-        } else if (lastStep.status) {
-          errorToast(lastStep.status, [], toastContext);
-        }
-      } else {
-        setTimeout(() => {
-          checkCeleryTask(celeryTaskId);
-        }, 3000);
-      }
-    }
-    catch (error: any) {
-      setTimeout(() => {
-        checkCeleryTask(celeryTaskId);
-      }, 5000);
-    }
-  };
-
 
   return (
     <>
