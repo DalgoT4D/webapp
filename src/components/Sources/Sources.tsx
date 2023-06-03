38ae8e3996056600dde149db952129eee85f901d
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-06-03 12:21:19 +0530

    delete source

--- src/components/Sources/Sources.tsx
+++ src/components/Sources/Sources.tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
 import useSWR from 'swr';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Box, Button } from '@mui/material';
 import { List } from '../List/List';
 import { backendUrl } from '@/config/constant';
+import { useSession } from 'next-auth/react';
+import { httpDelete } from '@/helpers/http';
 import CreateSourceForm from './CreateSourceForm';
 
 const headers = ['Source details', 'Type'];
 
 export const Sources = () => {
+  const { data: session }: any = useSession();
   const [rows, setRows] = useState<Array<Array<string>>>([]);
   const { data, isLoading, mutate } = useSWR(
     `${backendUrl}/api/airbyte/sources`
@@ -16,9 +19,22 @@ export const Sources = () => {
 
   useEffect(() => {
     if (data && data.length > 0) {
-      const rows = data.map((element: any) => [
-        element.name,
-        element.sourceDest,
+      const rows = data.map((source: any, idx: number) => [
+        source.name,
+        source.sourceDest,
+        [
+          <Box sx={{ justifyContent: 'end', display: 'flex' }} key={'box-' + idx}>
+            <Button
+              variant="contained"
+              onClick={() => deleteSource(source)}
+              key={'del-' + idx}
+              sx={{ backgroundColor: '#d84141' }}
+            >
+              Delete
+            </Button>
+          </Box>
+
+        ]
       ]);
       setRows(rows);
     }
@@ -30,6 +46,11 @@ export const Sources = () => {
 
   if (isLoading) {
     return <CircularProgress />;
+  }
+
+  const deleteSource = async (source: any) => {
+    await httpDelete(session, `airbyte/sources/${source.sourceId}`);
+    mutate();
   }
 
   return (
