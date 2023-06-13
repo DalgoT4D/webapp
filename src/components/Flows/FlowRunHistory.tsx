951278dbb07297aa4df7ba9c56b5dfe8dfab041e
Ishan <koradiaishan335@gmail.com>
2023-06-13 14:54:12 +0530

    null check

--- src/components/Flows/FlowRunHistory.tsx
+++ src/components/Flows/FlowRunHistory.tsx
@@ -85,91 +85,92 @@ const FlowRunHistory = ({
       </DialogTitle>
       <DialogContent sx={{ marginLeft: '20px', overflowX: 'hidden' }}>
         <Stack width="100rem">
-          {flowRuns.map((flowRun: any, idx: number) => (
-            <Box display="flex" gap="10px" key={idx}>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <Box
-                  sx={{
-                    height: '7px',
-                    width: '7px',
-                    borderRadius: '50%',
-                    backgroundColor:
-                      flowRun && flowRun.status === 'COMPLETED'
-                        ? '#399D47'
-                        : '#981F1F',
-                  }}
-                >
-                  <Typography color="white">i</Typography>
-                </Box>
-                <Divider orientation="vertical" />
-              </Box>
-              <Box
-                sx={{
-                  gap: '10px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  paddingTop: '10px',
-                  paddingBottom: '20px',
-                  width: '50%',
-                  wordWrap: 'break-word',
-                }}
-              >
-                <Typography>{lastRunTime(flowRun)}</Typography>
+          {flowRuns &&
+            flowRuns.map((flowRun: any, idx: number) => (
+              <Box display="flex" gap="10px" key={idx}>
                 <Box
                   sx={{
                     display: 'flex',
-                    gap: '10px',
+                    flexDirection: 'column',
                     alignItems: 'center',
                   }}
                 >
-                  <Typography sx={{ color: 'black', fontWeight: '700' }}>
-                    {flowRun['name']}
-                  </Typography>
-                  {flowRun['status'] === 'COMPLETED' ? (
-                    <TaskAltIcon
-                      sx={{
-                        alignItems: 'center',
-                        fontSize: 'medium',
-                        color: '#399D47',
-                      }}
-                    />
-                  ) : (
-                    <WarningAmberIcon
-                      sx={{
-                        alignItems: 'center',
-                        fontSize: 'medium',
-                        color: '#981F1F',
-                      }}
-                    />
-                  )}
-                  <Button onClick={() => handleShowMore(idx)}>
-                    {showLogs[idx] ? 'show less' : 'show more'}
-                  </Button>
+                  <Box
+                    sx={{
+                      height: '7px',
+                      width: '7px',
+                      borderRadius: '50%',
+                      backgroundColor:
+                        flowRun && flowRun.status === 'COMPLETED'
+                          ? '#399D47'
+                          : '#981F1F',
+                    }}
+                  >
+                    <Typography color="white">i</Typography>
+                  </Box>
+                  <Divider orientation="vertical" />
                 </Box>
-                <Collapse
+                <Box
                   sx={{
-                    width: '60%',
-                    backgroundColor: 'background.default',
+                    gap: '10px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    paddingTop: '10px',
+                    paddingBottom: '20px',
+                    width: '50%',
+                    wordWrap: 'break-word',
                   }}
-                  in={showLogs[idx]}
-                  timeout="auto"
-                  unmountOnExit
                 >
-                  <CardContent sx={{}}>
-                    {flowRun?.logs?.map((log: any, idx1: number) => (
-                      <Box key={idx1}>- {log?.message}</Box>
-                    ))}
-                  </CardContent>
-                </Collapse>
+                  <Typography>{lastRunTime(flowRun)}</Typography>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      gap: '10px',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Typography sx={{ color: 'black', fontWeight: '700' }}>
+                      {flowRun.name}
+                    </Typography>
+                    {flowRun.status === 'COMPLETED' ? (
+                      <TaskAltIcon
+                        sx={{
+                          alignItems: 'center',
+                          fontSize: 'medium',
+                          color: '#399D47',
+                        }}
+                      />
+                    ) : (
+                      <WarningAmberIcon
+                        sx={{
+                          alignItems: 'center',
+                          fontSize: 'medium',
+                          color: '#981F1F',
+                        }}
+                      />
+                    )}
+                    <Button onClick={() => handleShowMore(idx)}>
+                      {showLogs[idx] ? 'show less' : 'show more'}
+                    </Button>
+                  </Box>
+                  <Collapse
+                    sx={{
+                      width: '60%',
+                      backgroundColor: 'background.default',
+                    }}
+                    in={showLogs[idx]}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <CardContent sx={{}}>
+                      {flowRun?.logs?.map((log: any, idx1: number) => (
+                        <Box key={idx1}>- {log?.message}</Box>
+                      ))}
+                    </CardContent>
+                  </Collapse>
+                </Box>
               </Box>
-            </Box>
-          ))}
+            ))}
         </Stack>
       </DialogContent>
     </Dialog>
