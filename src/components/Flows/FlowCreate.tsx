9e79aeafabe310abecadeca4b0597bde512256d0
mdshamoon <mohdshamoon841@gmail.com>
2023-07-08 11:06:09 +0530

    added mutate

--- src/components/Flows/FlowCreate.tsx
+++ src/components/Flows/FlowCreate.tsx
@@ -149,6 +149,7 @@ const FlowCreate = ({
           toastContext
         );
         setSelectedFlow('');
+        mutate();
         updateCrudVal('index');
       } else {
         const blocks = data.connectionBlocks.map(
