eca0532218485266a0d9e39678b171aff4197cb9
Ishan <koradiaishan335@gmail.com>
2023-07-17 16:49:11 +0530

    not needed - using the helper

--- src/components/Destinations/EditDestinationForm.tsx
+++ src/components/Destinations/EditDestinationForm.tsx
@@ -99,85 +99,6 @@ const EditDestinationForm = ({
     }
   }, [showForm]);
 
-  const prePrepareConfigSpecs = (
-    result: any,
-    data: any,
-    parent = 'parent',
-    exclude: any[] = [],
-    dropdownEnums: any[] = []
-  ) => {
-    // Push the parent enum in the array
-    if (exclude.length > 0) {
-      if (exclude[0] in data?.properties) {
-        dropdownEnums.push(data?.properties[exclude[0]]?.const);
-      }
-    }
-
-    for (const [key, value] of Object.entries<any>(data?.properties || {})) {
-      // The parent oneOf key has already been added to the array
-      if (exclude.includes(key)) continue;
-
-      const objParentKey = `${parent}.${key}`;
-
-      if (value?.type === 'object') {
-        let commonField: string[] = [];
-
-        // Find common property among all array elements of 'oneOf' array
-        if (value['oneOf'] && value['oneOf'].length > 1) {
-          value['oneOf'].forEach((ele: any) => {
-            if (commonField.length > 0) {
-              commonField = ele?.required.filter((value: any) =>
-                commonField.includes(value)
-              );
-            } else {
-              commonField = ele?.required;
-            }
-          });
-        }
-
-        const objResult = {
-          field: `${objParentKey}.${commonField}`,
-          type: value?.type,
-          order: value?.order,
-          title: value?.title,
-          description: value?.description,
-          parent:
-            dropdownEnums.length > 0
-              ? dropdownEnums[dropdownEnums.length - 1]
-              : '',
-          enum: [],
-          specs: [],
-        };
-
-        result.push(objResult);
-
-        value?.oneOf.forEach((eachEnum: any) => {
-          prePrepareConfigSpecs(
-            objResult.specs,
-            eachEnum,
-            objParentKey,
-            commonField,
-            objResult.enum
-          );
-        });
-
-        continue;
-      }
-
-      result.push({
-        ...value,
-        field: objParentKey,
-        parent:
-          dropdownEnums.length > 0
-            ? dropdownEnums[dropdownEnums.length - 1]
-            : '',
-        required: data?.required.includes(key),
-      });
-    }
-
-    return result;
-  };
-
   useEffect(() => {
     if (watchSelectedDestinationDef?.id) {
       (async () => {
@@ -187,20 +108,24 @@ const EditDestinationForm = ({
             `airbyte/destination_definitions/${watchSelectedDestinationDef.id}/specifications`
           );
 
-          let connectorConfigInput = new ConnectorConfigInput('source', data);
+          let connectorConfigInput = new ConnectorConfigInput(
+            'destination',
+            data
+          );
+
+          console.log(
+            'warehouse connection config',
+            warehouse.connectionConfiguration
+          );
+
+          console.log();
 
           connectorConfigInput.setValidOrderToAllProperties();
 
           connectorConfigInput.setOrderToChildProperties();
 
           // Prepare the specs config before setting it
-          const specsConfigFields = prePrepareConfigSpecs(
-            [],
-            connectorConfigInput.specsData,
-            'config',
-            [],
-            []
-          );
+          const specsConfigFields = connectorConfigInput.prepareSpecsToRender();
 
           setDestinationDefSpecs(specsConfigFields);
 
