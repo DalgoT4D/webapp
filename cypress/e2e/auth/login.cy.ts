333c46ea80e488447f7fc723ea62f406a674216a
Ishan <koradiaishan335@gmail.com>
2023-07-28 13:10:11 +0530

    cypress testing setup and login flow

---
+++ cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,32 @@
+describe('The Home Page', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+  });
+
+  it('Load the login page', () => {
+    cy.get('h5').should('contain', 'Log In');
+  });
+
+  it('Validations', () => {
+    cy.get('[data-testid="submitbutton"]').click();
+    cy.get('p').should('contain', 'Password is required');
+    cy.get('p').should('contain', 'Business email is required');
+  });
+
+  it('Redirect to forgot password page', () => {
+    cy.contains('Forgot password?').click();
+    cy.get('h5').should('contain', 'Forgot password');
+  });
+
+  it('Redirect to Sign up', () => {
+    cy.contains('Sign Up').click();
+    cy.get('h5').should('contain', 'Create an account');
+  });
+
+  it('Successfully login', () => {
+    cy.get('[data-testid="username"]').type(Cypress.env('username'));
+    cy.get('[data-testid="password"]').type(Cypress.env('password'));
+    cy.get('[data-testid="submitbutton"]').click();
+    cy.get('div').should('contain', 'User logged in successfully');
+  });
+});
