b70ada73e7edc3c9bbf98b03cd8003564b88bd01
Ishan <koradiaishan335@gmail.com>
2023-08-02 11:56:46 +0530

    removing role mapping and updating apis to send role slug and removing the status column from everywhere

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,19 +1,1 @@
 export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-
-export const roles = [
-  {
-    id: 1,
-    slug: 'report_viewer',
-    label: 'Report Viewer',
-  },
-  {
-    id: 2,
-    slug: 'pipeline_manager',
-    label: 'Pipeline Manager',
-  },
-  {
-    id: 3,
-    slug: 'account_manager',
-    label: 'Account Manager',
-  },
-];
