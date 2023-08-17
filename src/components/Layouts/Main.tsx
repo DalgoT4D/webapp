32ddb354339c915a24b95db5fb1cfdc369afdc82
Ishan <koradiaishan335@gmail.com>
2023-08-17 13:56:19 +0530

    minor changes

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -12,12 +12,13 @@ export const Main = ({ children }: any) => {
   const router = useRouter();
 
   // Verify email with token should be visible whether the token is there or not
-  if (router.pathname === '/verifyemail' || router.pathname === '/invitations')
-    return children;
+  if (router.pathname === '/verifyemail') return children;
 
-  if (!session?.user.token)
-    // if the user is not logged in, return immediately
-    return children;
+  // invitations page should be accessible with or without the token
+  if (router.pathname === '/invitations') return children;
+
+  // if the user is not logged in, return immediately
+  if (!session?.user.token) return children;
 
   // User is logged in
   // if the user has not verified email redirect them to email verification page
