5c157fee67dc75607647279cc0c1f84c91abcf12
Ishan <koradiaishan335@gmail.com>
2023-11-10 15:05:46 +0530

    common log card for SingleFlowRunHistory used in pipeline overview and orchestrate page

--- src/components/Logs/LogCard.tsx
+++ src/components/Logs/LogCard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {
   Box,
+  Button,
   Card,
   CardActions,
   CardContent,
@@ -9,20 +10,40 @@ import {
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+export type FlowRunLogMessage = {
+  message: string;
+};
+
 interface LogCardPropss {
   expand: boolean;
-  logs: Array<string>;
+  logs: Array<string | FlowRunLogMessage>;
   setExpand: (...args: any) => any;
+  fetchMore?: boolean;
+  fetchMoreLogs?: () => void;
 }
 
-export const LogCard = ({ expand, setExpand, logs }: LogCardPropss) => {
+export const LogCard = ({
+  expand,
+  setExpand,
+  logs,
+  fetchMore = false,
+  fetchMoreLogs = () => {},
+}: LogCardPropss) => {
   return (
     <Card
       sx={{
+        gap: '10px',
+        display: 'flex',
+        flexDirection: 'column',
         marginTop: '10px',
-        padding: '4px',
+        paddingTop: '10px',
+        paddingBottom: '20px',
         borderRadius: '8px',
         color: '#092540',
+        backgroundColor: '#f5f5f5',
+        width: '100%',
+        wordWrap: 'break-word',
+        border: '1px solid #e0e0e0',
       }}
     >
       <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -37,10 +58,15 @@ export const LogCard = ({ expand, setExpand, logs }: LogCardPropss) => {
       </CardActions>
       <Collapse in={expand} unmountOnExit>
         {
-          <CardContent>
-            {logs?.map((logMessage: string, idx) => (
-              <Box key={idx}>{logMessage}</Box>
+          <CardContent data-testid="logmessages">
+            {logs?.map((log: any, idx) => (
+              <Box key={idx}>- {log?.message || log}</Box>
             ))}
+            {fetchMore && (
+              <Button data-testid="offset" onClick={() => fetchMoreLogs()}>
+                Fetch more
+              </Button>
+            )}
           </CardContent>
         }
       </Collapse>
