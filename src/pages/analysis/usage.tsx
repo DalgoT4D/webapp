491d59e1cf3a4f24d13b238141828189f144151d
Ishan <koradiaishan335@gmail.com>
2023-11-10 12:41:43 +0530

    try catch & adding contact us message if they have not subscribed to superset.

--- src/pages/analysis/usage.tsx
+++ src/pages/analysis/usage.tsx
@@ -1,21 +1,30 @@
 import { embedDashboard } from '@superset-ui/embedded-sdk';
 import styles from '@/styles/Home.module.css';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { PageHead } from '@/components/PageHead';
 import { httpPost } from '@/helpers/http';
 import { usageDashboardId, usageDashboardDomain } from '@/config/constant';
+import { GlobalContext } from '@/contexts/ContextProvider';
+import { errorToast } from '@/components/ToastMessage/ToastHelper';
+import { Box, Typography } from '@mui/material';
 
 export default function Usage() {
   const { data: session }: any = useSession();
+  const globalContext = useContext(GlobalContext);
 
   const fetchEmbedToken = async () => {
-    const { embed_token } = await httpPost(
-      session,
-      `superset/embed_token/${usageDashboardId}/`,
-      {}
-    );
-    return embed_token;
+    try {
+      const { embed_token } = await httpPost(
+        session,
+        `superset/embed_token/${usageDashboardId}/`,
+        {}
+      );
+      return embed_token;
+    } catch (err: any) {
+      console.error(err);
+      errorToast(err.message, [], globalContext);
+    }
   };
 
   useEffect(() => {
@@ -25,7 +34,7 @@ export default function Usage() {
         const mountHTMLElement: HTMLElement | null = document.getElementById(
           'dashboard-container'
         );
-        if (mountHTMLElement) {
+        if (mountHTMLElement && embed_token) {
           embedDashboard({
             id: usageDashboardId || '',
             supersetDomain: usageDashboardDomain || '',
@@ -48,7 +57,35 @@ export default function Usage() {
     <>
       <PageHead title="Dalgo" />
       <main className={styles.usage}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            sx={{ fontWeight: 700 }}
+            variant="h4"
+            gutterBottom
+            color="#000"
+          >
+            Usage Dashboard
+          </Typography>
+        </Box>
         <div id="dashboard-container"></div>
+        {!globalContext?.CurrentOrg?.state?.viz_url ? (
+          <>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 20 }}>
+              <Typography variant="h4" sx={{ alignContent: 'center' }}>
+                You have not subscribed to Superset for Visualisation.
+              </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+              <Typography variant="h6">
+                Please contact the Dalgo team at{' '}
+                <a href="mailto:support@dalgo.in">support@dalgo.in</a> for more
+                information
+              </Typography>
+            </Box>
+          </>
+        ) : (
+          <div id="dashboard-container"></div>
+        )}
       </main>
     </>
   );
