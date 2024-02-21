87431058b8060dc4baa5e25543af8f2425f26c43
Ishankoradia <ikoradia@umich.edu>
2024-02-21 14:57:42 +0530

    canvas header

--- src/components/TransformWorkflow/FlowEditor/Components/Canvas.tsx
+++ src/components/TransformWorkflow/FlowEditor/Components/Canvas.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider } from '@mui/material';
+import { Box, Button, Divider, Typography } from '@mui/material';
 import ReplayIcon from '@mui/icons-material/Replay';
 import React from 'react';
 import { useCallback, useState } from 'react';
@@ -19,6 +19,45 @@ import ReactFlow, {
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 
+const CanvasHeader = ({}) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%',
+        flexDirection: 'space-between',
+        justifyContent: 'center',
+        padding: '0 20px',
+        gap: '10px',
+      }}
+    >
+      <Typography variant="h6" sx={{ marginLeft: 'auto' }}>
+        Workflow01
+      </Typography>
+
+      <Box sx={{ marginLeft: 'auto', display: 'flex', gap: '20px' }}>
+        <Button
+          variant="contained"
+          type="button"
+          onClick={() =>
+            alert('This will run the workflow with or without parameters.')
+          }
+        >
+          Run
+        </Button>
+        <Button
+          variant="contained"
+          type="button"
+          onClick={() => alert('Not sure what this will do')}
+        >
+          Save
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
 const Canvas = ({}) => {
   const nodes: any = [];
   const edges: any = [];
@@ -30,33 +69,7 @@ const Canvas = ({}) => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Box sx={{ height: '10%', background: 'lightgrey' }}>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            height: '100%',
-            flexDirection: 'row-reverse',
-            padding: '0 20px',
-            gap: '10px',
-          }}
-        >
-          <Button
-            variant="contained"
-            type="button"
-            onClick={() =>
-              alert('This will run the workflow with or without parameters.')
-            }
-          >
-            Run
-          </Button>
-          <Button
-            variant="contained"
-            type="button"
-            onClick={() => alert('Not sure what this will do')}
-          >
-            Save
-          </Button>
-        </Box>
+        <CanvasHeader />
       </Box>
       <Divider orientation="horizontal" sx={{ color: 'black' }} />
       <Box sx={{ height: '90%' }}>
