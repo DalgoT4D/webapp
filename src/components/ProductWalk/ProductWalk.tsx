e5e46670fff599761641a9f565a39fea3fc59db0
Ishankoradia <ikoradia@umich.edu>
2024-02-23 18:54:01 +0530

    product walkthrough for the ingest page

---
+++ src/components/ProductWalk/ProductWalk.tsx
@@ -0,0 +1,37 @@
+import { primaryColor } from '@/config/theme';
+import React from 'react';
+import Joyride, { CallBackProps, STATUS, Step } from 'react-joyride';
+import { WalkThroughContent } from './WalkThroughContent';
+
+interface StepContent {
+  target: string;
+  body: string;
+}
+
+interface ProductWalkProps {
+  run: boolean;
+  steps: StepContent[];
+}
+
+export const ProductWalk = ({ run, steps }: ProductWalkProps) => {
+  return (
+    <Joyride
+      callback={() => {}}
+      continuous
+      hideCloseButton
+      run={run}
+      showProgress
+      showSkipButton
+      steps={steps.map((step) => ({
+        target: step.target,
+        content: <WalkThroughContent body={step.body} />,
+      }))}
+      styles={{
+        options: {
+          zIndex: 10000,
+          primaryColor: primaryColor,
+        },
+      }}
+    />
+  );
+};
