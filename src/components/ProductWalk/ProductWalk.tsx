d7739f7fd3d48a822d116527265b7ed4c136c241
Ishankoradia <ikoradia@umich.edu>
2024-02-27 22:50:47 +0530

    minor changes; added env var to enable/disbale the feature

--- src/components/ProductWalk/ProductWalk.tsx
+++ src/components/ProductWalk/ProductWalk.tsx
@@ -1,7 +1,16 @@
 import { primaryColor } from '@/config/theme';
 import React from 'react';
-import Joyride, { CallBackProps, STATUS, Step } from 'react-joyride';
+import Joyride, {
+  ACTIONS,
+  EVENTS,
+  ORIGIN,
+  STATUS,
+  CallBackProps,
+  TooltipProps,
+  TooltipRenderProps,
+} from 'react-joyride';
 import { WalkThroughContent } from './WalkThroughContent';
+import { Box, Button, Tooltip } from '@mui/material';
 
 interface StepContent {
   target: string;
@@ -10,25 +19,32 @@ interface StepContent {
 
 interface ProductWalkProps {
   run: boolean;
+  setRun: (...args: any) => any;
   steps: StepContent[];
 }
 
-export const ProductWalk = ({ run, steps }: ProductWalkProps) => {
+export const ProductWalk = ({ run, steps, setRun }: ProductWalkProps) => {
+  const handleJoyrideCallback = (data: CallBackProps) => {
+    const { action, index, origin, status, type } = data;
+  };
+
   return (
     <Joyride
-      callback={() => {}}
-      hideCloseButton
+      callback={handleJoyrideCallback}
       run={run}
+      continuous
       showProgress
       showSkipButton
-      steps={steps.map((step) => ({
+      steps={steps.map((step: StepContent, idx: number) => ({
         target: step.target,
         content: <WalkThroughContent body={step.body} />,
+        placementBeacon: 'right',
       }))}
       styles={{
         options: {
           zIndex: 10000,
           primaryColor: primaryColor,
+          width: '300px',
         },
       }}
     />
