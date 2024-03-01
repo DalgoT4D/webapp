bfcaa8b3130e28e517c4fbfe68c09971535a27cd
Ishankoradia <ikoradia@umich.edu>
2024-03-01 19:17:40 +0530

    use the graph api to set the initial nodes and edges

--- src/components/TransformWorkflow/FlowEditor/Components/Nodes/OperationNode.tsx
+++ src/components/TransformWorkflow/FlowEditor/Components/Nodes/OperationNode.tsx
@@ -1,6 +1,15 @@
 import { Box } from '@mui/material';
 import 'react';
+import { Handle, Position, useReactFlow, useNodeId } from 'reactflow';
 
-export function OperationNode() {
-  return <Box>Operation node</Box>;
+export function OperationNode({ data }: any) {
+  return (
+    <Box>
+      {data?.node?.config?.type}
+      <>
+        <Handle type="target" position={Position.Left} />
+        <Handle type="source" position={Position.Right} />
+      </>
+    </Box>
+  );
 }
