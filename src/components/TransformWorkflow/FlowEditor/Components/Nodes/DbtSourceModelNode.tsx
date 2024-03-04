35bf0f319917bb305990fcb75bd5619849e67e99
Ishankoradia <ikoradia@umich.edu>
2024-03-04 19:00:10 +0530

    fetching columns and data types ; some more css

--- src/components/TransformWorkflow/FlowEditor/Components/Nodes/DbtSourceModelNode.tsx
+++ src/components/TransformWorkflow/FlowEditor/Components/Nodes/DbtSourceModelNode.tsx
@@ -7,18 +7,107 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
-import { useState } from 'react';
-import { Handle, Position, useNodeId } from 'reactflow';
-import { Close } from '@mui/icons-material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useMemo, useState } from 'react';
+import { Handle, Position, useNodeId, useEdges, Edge } from 'reactflow';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { SrcModelNodeType } from '../Canvas';
+import { httpGet } from '@/helpers/http';
+import { useSession } from 'next-auth/react';
+
+interface ColumnData {
+  name: string;
+  data_type: string;
+}
+
+const NodeDataTableComponent = ({ columns }: { columns: ColumnData[] }) => {
+  return (
+    <Table sx={{ borderSpacing: '0px' }}>
+      <TableBody>
+        <TableRow
+          sx={{
+            boxShadow: 'none',
+            background: '#F5F5F5',
+          }}
+          key={'NAME'}
+        >
+          <TableCell
+            sx={{
+              padding: '4px 0px 4px 10px',
+              fontWeight: 600,
+              color: '#212121',
+            }}
+            align="left"
+          >
+            NAME
+          </TableCell>
+          <TableCell
+            sx={{
+              padding: '4px 0px 4px 10px',
+              width: '40%',
+              borderLeft: '1px solid #F8F8F8',
+              fontWeight: 600,
+              color: '#212121',
+            }}
+            align="left"
+          >
+            DATA
+          </TableCell>
+        </TableRow>
+        {columns.map((row: ColumnData, idx: number) => (
+          <TableRow
+            sx={{
+              boxShadow: 'none',
+              background: idx % 2 === 0 ? '#E1E1E1' : '#F5F5F5',
+              fontSize: '11px',
+            }}
+            key={row.name}
+          >
+            <TableCell
+              sx={{
+                padding: '4px 0px 4px 10px',
+                color: '#212121',
+                fontWeight: 500,
+              }}
+              align="left"
+            >
+              {row.name}
+            </TableCell>
+            <TableCell
+              sx={{
+                padding: '4px 0px 4px 10px',
+                width: '40%',
+                borderLeft: '1px solid #F8F8F8',
+                color: '#212121',
+                fontWeight: 500,
+              }}
+              align="left"
+            >
+              {row.data_type}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
 
 export function DbtSourceModelNode(node: SrcModelNodeType) {
-  const { data } = node;
+  const { data: session } = useSession();
   const [openOperationList, setOpenOperationList] = useState(false);
+  const [columns, setColumns] = useState<Array<any>>([]);
   const [operationSlug, setOperationSlug] = useState<string | null>(null);
 
+  const edges = useEdges();
   const nodeId: string | null = useNodeId();
+  const { data } = node;
+
+  // can only this node if it doesn't have anything emanating from it i.e. leaf node
+  const isDeletable: boolean = edges.find(
+    (edge: Edge) => edge.source === nodeId
+  )
+    ? false
+    : true;
 
   const handleDeleteAction = () => {
     data.triggerDelete(nodeId);
@@ -39,13 +128,19 @@ export function DbtSourceModelNode(node: SrcModelNodeType) {
     setOpenOperationList(false);
   };
 
-  const rows = [
-    { name: 'Month', dataType: 'string' },
-    { name: 'ngo', dataType: 'string' },
-    { name: 'spoc', dataType: 'string' },
-    { name: 'measure', dataType: 'string' },
-    { name: 'Indicator', dataType: 'string' },
-  ];
+  useMemo(() => {
+    (async () => {
+      try {
+        const data: ColumnData[] = await httpGet(
+          session,
+          `warehouse/table_columns/${node.data.node.schema}/${node.data.node.input_name}`
+        );
+        setColumns(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [session, node.data.node.schema, node.data.node.input_name]);
 
   const operations = [
     { label: 'Flatten', slug: 'flatte' },
@@ -60,20 +155,21 @@ export function DbtSourceModelNode(node: SrcModelNodeType) {
   ];
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', width: '250px' }}>
+      {!openOperationList && (
+        <>
+          <Handle type="target" position={Position.Left} />
+          <Handle type="source" position={Position.Right} />
+        </>
+      )}
       <Box
         sx={{
           borderRadius: '5px',
           display: 'flex',
           flexDirection: 'column',
+          width: '100%',
         }}
       >
-        {!openOperationList && (
-          <>
-            <Handle type="target" position={Position.Left} />
-            <Handle type="source" position={Position.Right} />
-          </>
-        )}
         <Box
           sx={{
             background: '#C3C3C3',
@@ -81,6 +177,7 @@ export function DbtSourceModelNode(node: SrcModelNodeType) {
             borderRadius: '4px 4px 0px 0px',
             alignItems: 'center',
             padding: '0px 10px',
+            gap: '30px',
           }}
         >
           <Box>
@@ -89,16 +186,21 @@ export function DbtSourceModelNode(node: SrcModelNodeType) {
               fontWeight={700}
               onClick={handlePreviewAction}
             >
-              {`${data.node.schema}.${data.node.input_name}`}
+              {`${data.node.input_name}`}
             </Typography>
           </Box>
           <Box sx={{ marginLeft: 'auto' }}>
-            <IconButton onClick={handleDeleteAction} data-testid="closebutton">
-              <Close />
-            </IconButton>
+            {isDeletable && (
+              <IconButton
+                onClick={handleDeleteAction}
+                data-testid="closebutton"
+              >
+                <DeleteIcon fontSize="small" />
+              </IconButton>
+            )}
             <IconButton
               onClick={handleOpenOperationList}
-              data-testid="closebutton"
+              data-testid="openoperationlist"
             >
               <ChevronRightIcon />
             </IconButton>
@@ -111,76 +213,29 @@ export function DbtSourceModelNode(node: SrcModelNodeType) {
             flexDirection: 'column',
             borderRadius: '0px 0px 4px 4px',
             height: '120px',
+            boxShadow: '0px 2px 4px 0px rgba(0, 0, 0, 0.16)',
             overflow: 'auto',
+            width: '100%',
           }}
         >
-          <Table sx={{ borderSpacing: '0px' }}>
-            <TableBody>
-              <TableRow
-                sx={{
-                  boxShadow: 'none',
-                  background: '#F5F5F5',
-                }}
-                key={'NAME'}
-              >
-                <TableCell
-                  sx={{
-                    padding: '4px 0px 4px 10px',
-                    fontWeight: 600,
-                    color: '#212121',
-                  }}
-                  align="left"
-                >
-                  NAME
-                </TableCell>
-                <TableCell
-                  sx={{
-                    padding: '4px 0px 4px 10px',
-                    width: '40%',
-                    borderLeft: '1px solid #F8F8F8',
-                    fontWeight: 600,
-                    color: '#212121',
-                  }}
-                  align="left"
-                >
-                  DATA
-                </TableCell>
-              </TableRow>
-              {rows.map((row, idx: number) => (
-                <TableRow
-                  sx={{
-                    boxShadow: 'none',
-                    background: idx % 2 === 0 ? '#E1E1E1' : '#F5F5F5',
-                    fontSize: '11px',
-                  }}
-                  key={row.name}
-                >
-                  <TableCell
-                    sx={{
-                      padding: '4px 0px 4px 10px',
-                      color: '#212121',
-                      fontWeight: 500,
-                    }}
-                    align="left"
-                  >
-                    {row.name}
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      padding: '4px 0px 4px 10px',
-                      width: '40%',
-                      borderLeft: '1px solid #F8F8F8',
-                      color: '#212121',
-                      fontWeight: 500,
-                    }}
-                    align="left"
-                  >
-                    {row.dataType}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          {columns.length > 0 ? (
+            <Box>
+              <NodeDataTableComponent columns={columns} />
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '120px',
+                width: '100%',
+              }}
+            >
+              <Typography>Columns not available</Typography>
+            </Box>
+          )}
         </Box>
       </Box>
       {openOperationList && (
