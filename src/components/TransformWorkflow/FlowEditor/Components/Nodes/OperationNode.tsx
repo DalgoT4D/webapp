214cf6fd34d0288ee27070ad3b67d9eb65443583
Ishankoradia <ikoradia@umich.edu>
2024-03-04 22:55:42 +0530

    run the workflow and deleting the node via api and redraw the graph via the api

--- src/components/TransformWorkflow/FlowEditor/Components/Nodes/OperationNode.tsx
+++ src/components/TransformWorkflow/FlowEditor/Components/Nodes/OperationNode.tsx
@@ -1,9 +1,32 @@
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Typography } from '@mui/material';
 import 'react';
-import { Handle, Position, useReactFlow, useNodeId } from 'reactflow';
+import {
+  Handle,
+  Position,
+  useReactFlow,
+  useNodeId,
+  useEdges,
+  Edge,
+} from 'reactflow';
 import { OperationNodeType } from '../Canvas';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export function OperationNode(node: OperationNodeType) {
+  const edges = useEdges();
+  const nodeId = useNodeId();
+
+  const { data, type } = node;
+  // can only this node if it doesn't have anything emanating edge from it i.e. leaf node
+  const isDeletable: boolean = edges.find(
+    (edge: Edge) => edge.source === nodeId
+  )
+    ? false
+    : true;
+
+  const handleDeleteAction = () => {
+    data.triggerDelete(nodeId, type);
+  };
+
   return (
     <Box>
       <>
@@ -31,7 +54,16 @@ export function OperationNode(node: OperationNodeType) {
               background: '#D9D9D9',
               borderRadius: '4px',
             }}
-          ></Box>
+          >
+            {isDeletable && (
+              <IconButton
+                onClick={handleDeleteAction}
+                data-testid="closebutton"
+              >
+                <DeleteIcon fontSize="small" />
+              </IconButton>
+            )}
+          </Box>
         </Box>
         <Divider orientation="horizontal" sx={{ color: '#EEEEEE' }} />
         <Box>
