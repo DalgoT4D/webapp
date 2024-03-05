9a88d3161278c1efea00f66c629c7d571f0558cb
Ishankoradia <ikoradia@umich.edu>
2024-03-05 17:38:19 +0530

    inital layout setup for the operation config forms

--- src/components/TransformWorkflow/FlowEditor/Components/OperationConfigForms/constant.ts
+++ src/components/TransformWorkflow/FlowEditor/Components/OperationConfigForms/constant.ts
@@ -1,11 +1,21 @@
+export const RENAME_COLUMNS_OP = 'renamecolumns';
+export const FLATTEN_OP = 'flatten';
+export const FLATTEN_JSON_OP = 'flattenjson';
+export const CAST_DATA_TYPES_OP = 'castdatatypes';
+export const COALESCE_COLUMNS_OP = 'coalescecolumns';
+export const ARITHMETIC_OP = 'arithmetic';
+export const CONCAT_COLUMNS_OP = 'concat';
+export const DROP_COLUMNS_OP = 'dropcolumns';
+export const REGEX_EXTRACTION_OP = 'regexextraction';
+
 export const operations = [
-  { label: 'Flatten', slug: 'flatten' },
-  { label: 'Flatten json', slug: 'flattenjson' },
-  { label: 'Cast data type', slug: 'castdatatypes' },
-  { label: 'Coalesce columns', slug: 'coalescecolumns' },
-  { label: 'Arithmetic', slug: 'arithmetic' },
-  { label: 'Concat', slug: 'concat' },
-  { label: 'Drop columns', slug: 'dropcolumns' },
-  { label: 'Rename columns', slug: 'renamecolumns' },
-  { label: 'Regex extraction', slug: 'regexextraction' },
+  { label: 'Flatten', slug: FLATTEN_OP },
+  { label: 'Flatten json', slug: FLATTEN_JSON_OP },
+  { label: 'Cast data type', slug: CAST_DATA_TYPES_OP },
+  { label: 'Coalesce columns', slug: COALESCE_COLUMNS_OP },
+  { label: 'Arithmetic', slug: ARITHMETIC_OP },
+  { label: 'Concat', slug: CONCAT_COLUMNS_OP },
+  { label: 'Drop columns', slug: DROP_COLUMNS_OP },
+  { label: 'Rename columns', slug: RENAME_COLUMNS_OP },
+  { label: 'Regex extraction', slug: REGEX_EXTRACTION_OP },
 ];
--- src/components/TransformWorkflow/FlowEditor/Components/OperationConfigLayout.tsx
+++ src/components/TransformWorkflow/FlowEditor/Components/OperationConfigLayout.tsx
@@ -1,19 +1,69 @@
-import { Box, SxProps } from '@mui/material';
+import { Box, Divider, SxProps, Typography } from '@mui/material';
 import React from 'react';
+import { OperationNodeType, SrcModelNodeType, UIOperationType } from './Canvas';
+import { operations } from './OperationConfigForms/constant';
+import InfoIcon from '@mui/icons-material/Info';
+import { RENAME_COLUMNS_OP } from './OperationConfigForms/constant';
+import RenameColumnOpForm from './OperationConfigForms/RenameColumnOpForm';
 
 interface OperationConfigProps {
   sx: SxProps;
   openConfigPanel: boolean;
   setOpenConfigPanel: (...args: any) => void;
+  operation: UIOperationType | null;
+  node: SrcModelNodeType | OperationNodeType | null;
 }
 
+interface OperationFormProps {
+  node: SrcModelNodeType | OperationNodeType;
+  operation: UIOperationType;
+}
+
+const operationComponentMapping: any = {
+  [RENAME_COLUMNS_OP]: ({ node, operation }: OperationFormProps) => (
+    <RenameColumnOpForm node={node} operation={operation} />
+  ), // add more operations here
+};
+
+const OperationForm = ({ operation, node }: OperationFormProps) => {
+  return operationComponentMapping[operation.slug]({ operation, node });
+};
+
 const OperationConfigLayout = ({
+  operation,
+  node,
   openConfigPanel,
   setOpenConfigPanel,
   sx,
 }: OperationConfigProps) => {
   if (!openConfigPanel) return null;
-  return <Box sx={{ ...sx }}>Operation config form</Box>;
+
+  if (!operation) return <Box sx={{ ...sx }}>Operation not found</Box>;
+
+  if (!node) return <Box sx={{ ...sx }}>Please select a node</Box>;
+
+  console.log('Selected this node in config form', node);
+  console.log('Selected this operation in config form', operation);
+  return (
+    <Box sx={{ ...sx }}>
+      <Box
+        sx={{
+          padding: '6px 16px 6px 16px',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Typography fontWeight={600} fontSize="15px" color="#5E5E5E">
+          {operation.label}
+        </Typography>
+        <Box sx={{ width: '16px', height: '16px' }}>
+          <InfoIcon sx={{ color: '#D9D9D9' }} />
+        </Box>
+      </Box>
+      <Divider orientation="horizontal" />
+      <OperationForm operation={operation} node={node} />
+    </Box>
+  );
 };
 
 export default OperationConfigLayout;
