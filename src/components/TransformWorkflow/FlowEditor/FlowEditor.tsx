4f9e9cf6df9ad51d36fa6393311e788dc5057d2b
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-03-10 21:52:12 +0530

    DbtRunLogs context and provider poll for logs by flow-run-id in Canvas component store using context provider todo: access and display in the preview pane

--- src/components/TransformWorkflow/FlowEditor/FlowEditor.tsx
+++ src/components/TransformWorkflow/FlowEditor/FlowEditor.tsx
@@ -6,6 +6,7 @@ import PreviewPane from './Components/PreviewPane';
 import { httpGet } from '@/helpers/http';
 import { useSession } from 'next-auth/react';
 import { SRC_MODEL_NODE } from './constant';
+import { DbtRunLogsProvider } from '@/contexts/DbtRunLogsContext';
 
 export type DbtSourceModel = {
   source_name: string;
@@ -46,22 +47,24 @@ const FlowEditor = ({}) => {
         paddingTop: '3.5rem',
       }}
     >
-      <Box sx={{ display: 'flex', height: '70%', overflow: 'inherit' }}>
-        <Box sx={{ width: '20%' }}>
-          <ProjectTree dbtSourceModels={sourcesModels} />
+      <DbtRunLogsProvider>
+        <Box sx={{ display: 'flex', height: '70%', overflow: 'inherit' }}>
+          <Box sx={{ width: '20%' }}>
+            <ProjectTree dbtSourceModels={sourcesModels} />
+          </Box>
+          <Divider orientation="vertical" sx={{ color: 'black' }} />
+          <Box sx={{ width: '80%' }}>
+            <Canvas
+              redrawGraph={refreshEditor}
+              setRedrawGraph={setRefreshEditor}
+            />
+          </Box>
         </Box>
-        <Divider orientation="vertical" sx={{ color: 'black' }} />
-        <Box sx={{ width: '80%' }}>
-          <Canvas
-            redrawGraph={refreshEditor}
-            setRedrawGraph={setRefreshEditor}
-          />
+        <Divider orientation="horizontal" sx={{ color: 'black' }} />
+        <Box sx={{ height: '30%', overflow: 'auto' }}>
+          <PreviewPane />
         </Box>
-      </Box>
-      <Divider orientation="horizontal" sx={{ color: 'black' }} />
-      <Box sx={{ height: '30%', overflow: 'auto' }}>
-        <PreviewPane />
-      </Box>
+      </DbtRunLogsProvider>
     </Box>
   );
 };
