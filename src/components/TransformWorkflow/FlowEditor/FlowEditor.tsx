270e79f67e1e1dacc0f228b48f08659bcf3a1e28
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-03-11 16:02:59 +0530

    first version of showing run logs

--- src/components/TransformWorkflow/FlowEditor/FlowEditor.tsx
+++ src/components/TransformWorkflow/FlowEditor/FlowEditor.tsx
@@ -6,7 +6,8 @@ import PreviewPane from './Components/PreviewPane';
 import { httpGet } from '@/helpers/http';
 import { useSession } from 'next-auth/react';
 import { SRC_MODEL_NODE } from './constant';
-import { DbtRunLogsProvider } from '@/contexts/DbtRunLogsContext';
+
+import { useDbtRunLogs } from '@/contexts/DbtRunLogsContext';
 
 export type DbtSourceModel = {
   source_name: string;
@@ -22,6 +23,7 @@ const FlowEditor = ({}) => {
   const [sourcesModels, setSourcesModels] = useState<DbtSourceModel[]>([]);
   const [refreshEditor, setRefreshEditor] = useState<boolean>(false);
   const [selectedTab, setSelectedTab] = useState(0);
+  const dbtRunLogs = useDbtRunLogs();
 
   const fetchSourcesModels = async () => {
     try {
@@ -52,38 +54,57 @@ const FlowEditor = ({}) => {
         paddingTop: '3.5rem',
       }}
     >
-      <DbtRunLogsProvider>
-        <Box sx={{ display: 'flex', height: '60%', overflow: 'inherit' }}>
-          <Box sx={{ width: '20%' }}>
-            <ProjectTree dbtSourceModels={sourcesModels} />
+      <Box sx={{ display: 'flex', height: '60%', overflow: 'inherit' }}>
+        <Box sx={{ width: '20%' }}>
+          <ProjectTree dbtSourceModels={sourcesModels} />
+        </Box>
+        <Divider orientation="vertical" sx={{ color: 'black' }} />
+        <Box sx={{ width: '80%' }}>
+          <Canvas
+            redrawGraph={refreshEditor}
+            setRedrawGraph={setRefreshEditor}
+          />
+        </Box>
+      </Box>
+      <Divider orientation="horizontal" sx={{ color: 'black' }} />
+      <Box>
+        <Tabs
+          value={selectedTab}
+          onChange={handleTabChange}
+          sx={{ display: 'flex', height: '4rem', alignItems: 'center' }}
+        >
+          <Tab label="Preview" />
+          <Tab label="Logs" />
+        </Tabs>
+      </Box>
+      <Divider orientation="horizontal" sx={{ color: 'black' }} />
+      <Box sx={{ height: '40%', overflow: 'auto' }}>
+        {selectedTab === 0 && <PreviewPane />}
+        {selectedTab === 1 && (
+          <Box sx={{ padding: '1rem' }}>
+            {dbtRunLogs.length > 0 ? (
+              dbtRunLogs.map((log, index) => (
+                <Box
+                  key={index}
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box sx={{ fontWeight: 700, minWidth: '25%' }}>
+                    {new Date(log.timestamp).toTimeString()}
+                  </Box>
+                  <Box sx={{ color: 'blue', textAlign: 'left', width: '100%' }}>
+                    {log.message}
+                  </Box>
+                </Box>
+              ))
+            ) : (
+              <Box>No logs available</Box>
+            )}
           </Box>
-          <Divider orientation="vertical" sx={{ color: 'black' }} />
-          <Box sx={{ width: '80%' }}>
-            <Canvas
-              redrawGraph={refreshEditor}
-              setRedrawGraph={setRefreshEditor}
-            />
-          </Box>
-        </Box>
-        <Divider orientation="horizontal" sx={{ color: 'black' }} />
-        <Box>
-          <Tabs
-            value={selectedTab}
-            onChange={handleTabChange}
-            sx={{ display: 'flex', height: '4rem', alignItems: 'center' }}
-          >
-            <Tab label="Preview" />
-            <Tab label="Logs" />
-          </Tabs>
-        </Box>
-        <Divider orientation="horizontal" sx={{ color: 'black' }} />
-        <Box sx={{ height: '40%', overflow: 'auto' }}>
-          {selectedTab === 0 && <PreviewPane />}
-          {selectedTab === 1 && (
-            <Box sx={{ padding: '1rem' }}>{/* Logs content goes here */}</Box>
-          )}
-        </Box>
-      </DbtRunLogsProvider>
+        )}
+      </Box>
     </Box>
   );
 };
