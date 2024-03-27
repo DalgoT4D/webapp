183f0911486eb5bae81ddb1c875531671e1f3bfd
Abhishek-N <abhisheknshetty97@gmail.com>
2024-03-27 15:27:13 +0530

    update tooltip component

--- src/components/UI/Tooltip/Tooltip.tsx
+++ src/components/UI/Tooltip/Tooltip.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import InfoIcon from '@mui/icons-material/Info';
+import Typography from '@mui/material/Typography';
 
 interface InfoTooltipProps {
   title: string;
@@ -8,7 +9,7 @@ interface InfoTooltipProps {
 
 function InfoTooltip({ title }: InfoTooltipProps) {
   return (
-    <Tooltip title={title}>
+    <Tooltip title={<Typography sx={{ fontSize: '14px' }}>{title}</Typography>}>
       <InfoIcon fontSize="small" sx={{ color: '#888888' }} />
     </Tooltip>
   );
