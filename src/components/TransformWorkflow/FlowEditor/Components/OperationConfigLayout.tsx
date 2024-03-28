87cfa581d8cbc7fab615d16ad3a9f969a751c4dd
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-03-28 18:51:13 +0530

    tooltip tweaks

--- src/components/TransformWorkflow/FlowEditor/Components/OperationConfigLayout.tsx
+++ src/components/TransformWorkflow/FlowEditor/Components/OperationConfigLayout.tsx
@@ -452,11 +452,11 @@ const OperationConfigLayout = ({
               <InfoTooltip
                 title={
                   selectedOp && selectedOp.slug === AGGREGATE_OP
-                    ? 'Performs a calculation on multiple values in a column and returns a new column with that value in every row.'
+                    ? 'Performs a calculation on multiple values in a column and returns a new column with that value in every row'
                     : selectedOp && selectedOp.slug === ARITHMETIC_OP
-                    ? 'Perform arithmetic operations on or between one or more columns.'
+                    ? 'Perform arithmetic operations on or between one or more columns'
                     : selectedOp && selectedOp.slug === CASEWHEN_OP
-                    ? 'Create one or more conditional statements (cases) on one or more columns.'
+                    ? 'Select the relevant column, operation, and comparison column or value'
                     : selectedOp && selectedOp.slug === CAST_DATA_TYPES_OP
                     ? "Convert a column's values (of any type) into a specified datatype"
                     : selectedOp && selectedOp.slug === COALESCE_COLUMNS_OP
@@ -464,22 +464,22 @@ const OperationConfigLayout = ({
                     : selectedOp && selectedOp.slug === DROP_COLUMNS_OP
                     ? 'Select the columns that you would like to remove from the table'
                     : selectedOp && selectedOp.slug === WHERE_OP
-                    ? 'Filters all the row values in the selected column based on the defined condition.'
+                    ? 'Filters all the row values in the selected column based on the defined condition'
                     : selectedOp && selectedOp.slug === FLATTEN_JSON_OP
                     ? 'Transforms JSON formatted data into Tablular formatted data'
                     : selectedOp && selectedOp.slug === GROUPBY_OP
-                    ? 'Group your data by one or more dimensions and analyse it.'
+                    ? 'Group your data by one or more dimensions and analyse it'
                     : selectedOp && selectedOp.slug === JOIN_OP
                     ? 'Combine rows from two or more tables, based on a related (key) column between them'
                     : selectedOp && selectedOp.slug === RENAME_COLUMNS_OP
                     ? 'Select columns and rename them'
                     : selectedOp && selectedOp.slug === REPLACE_COLUMN_VALUE_OP
-                    ? 'Replace all the row values in a column a specified string with a new value'
+                    ? 'Replace all the row values in a column having a specified string with a new value'
                     : selectedOp && selectedOp.slug === UNION_OP
                     ? 'Combine data for matching columns across two datasets'
                     : selectedOp && selectedOp.label === 'Create Output Table'
                     ? 'Generate a table which will be saved with a new name in your desired warehouse schema'
-                    : 'Select a function to learn how you can use it to transform your data.'
+                    : 'Select a function to learn how you can use it to transform your data'
                 }
               />
             </Box>
