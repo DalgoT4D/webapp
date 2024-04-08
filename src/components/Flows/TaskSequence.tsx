79f744aa4d9f48ca2485b90cda3867705735f587
mdshamoon <mohdshamoon841@gmail.com>
2024-04-08 16:57:29 +0530

    added remove node

--- src/components/Flows/TaskSequence.tsx
+++ src/components/Flows/TaskSequence.tsx
@@ -1,7 +1,8 @@
 import { ControllerRenderProps } from 'react-hook-form';
 import { Autocomplete } from '../UI/Autocomplete/Autocomplete';
 import { TransformTask } from '../DBT/DBTTarget';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useRef } from 'react';
+import { DeleteOutlineOutlined } from '@mui/icons-material';
 
 import { Tree } from 'react-arborist';
 import { Box } from '@mui/material';
@@ -11,55 +12,21 @@ interface TaskSequenceProps {
   options: TransformTask[];
 }
 
-function Node({ node, dragHandle }: any) {
-  console.log(node);
-  /* This node instance can do many things. See the API reference. */
-  return (
-    <Box
-      ref={dragHandle}
-      sx={{
-        display: 'flex',
-        borderRadius: '6px',
-        overflow: 'hidden',
-        fontWeight: 600,
-      }}
-    >
-      <Box
-        sx={{
-          p: '4px 12px',
-          width: '30px',
-          background: '#33A195',
-          color: 'white',
-        }}
-      >
-        {node.rowIndex + 1}
-      </Box>
-      <Box sx={{ p: '4px 12px', background: '#F5FAFA', width: '100%' }}>
-        {' '}
-        {node.data.name}
-      </Box>
-      <Box
-        sx={{
-          p: '4px 12px',
-          marginLeft: 'auto',
-          width: '80px',
-          background: '#33A195',
-          color: 'white',
-        }}
-      >
-        {node.data.type}
-      </Box>
-    </Box>
-  );
-}
-
 export const TaskSequence = ({
   field,
   options: initialOptions,
 }: TaskSequenceProps) => {
-  const [selectedOptions, setSelectedOptions] = useState<TransformTask[]>([]);
-  const [autocompleteOptions, setAutocompleteOptions] =
-    useState(initialOptions);
+  const treeRef = useRef();
+  const [selectedOptions, setSelectedOptions] = useState<TransformTask[]>(
+    initialOptions
+      .filter((option) => option.generated_by === 'system')
+      .map((option) => ({ id: option.seq, ...option }))
+  );
+  const [autocompleteOptions, setAutocompleteOptions] = useState(
+    initialOptions
+      .filter((option) => option.generated_by !== 'system')
+      .map((option) => ({ id: option.seq, ...option }))
+  );
 
   const handleSelect = (event, value) => {
     if (value) {
@@ -73,31 +40,85 @@ export const TaskSequence = ({
       setAutocompleteOptions(newAutocompleteOptions);
     }
   };
+  const removeNode = (node: any) => {
+    const newAutocompleteOptions = [...autocompleteOptions, node.data].sort(
+      (a, b) => a.seq - b.seq
+    );
 
-  const data = useMemo(
-    () =>
-      selectedOptions.map((option) => ({
-        id: option.uuid,
-        name: option.label,
-        type: option.generated_by,
-      })),
-    [selectedOptions]
-  );
+    const newSelectedOptions = selectedOptions.filter(
+      (option) => option.uuid !== node.data.uuid
+    );
+
+    setSelectedOptions(newSelectedOptions);
+    setAutocompleteOptions(newAutocompleteOptions);
+  };
+
+  function Node({ node, dragHandle }: any) {
+    /* This node instance can do many things. See the API reference. */
+    return (
+      <Box
+        ref={dragHandle}
+        sx={{
+          maxWidth: '500px',
+          display: 'flex',
+          borderRadius: '6px',
+          overflow: 'hidden',
+          fontWeight: 600,
+        }}
+      >
+        <Box
+          sx={{
+            p: '4px 12px',
+            width: '30px',
+            background: '#33A195',
+            color: 'white',
+          }}
+        >
+          {node.rowIndex + 1}
+        </Box>
+        <Box sx={{ p: '4px 12px', background: '#F5FAFA', width: '100%' }}>
+          {node.data.command}
+        </Box>
+        <Box
+          sx={{
+            p: '4px 12px',
+            marginLeft: 'auto',
+            width: '80px',
+            background: '#33A195',
+            color: 'white',
+            borderRadius: '0px 6px 6px 0px',
+          }}
+        >
+          {node.data.generated_by}
+        </Box>
+        <Box
+          sx={{ cursor: 'pointer', p: '2px' }}
+          onClick={() => removeNode(node)}
+        >
+          <DeleteOutlineOutlined />
+        </Box>
+      </Box>
+    );
+  }
 
   return (
     <>
       <Autocomplete
         inputValue=""
+        getOptionLabel={(task: any) => task.command}
         placeholder="Select"
         options={autocompleteOptions}
         onChange={handleSelect}
       />
       <Tree
-        data={data}
-        openByDefault={false}
+        ref={treeRef}
+        data={selectedOptions}
+        idAccessor="uuid"
+        onMove={(node) => console.log(node, treeRef.current)}
+        disableDrag={(data) => data.generated_by === 'system'}
         width={'100%'}
         indent={32}
-        rowHeight={36}
+        rowHeight={50}
         overscanCount={1}
         paddingTop={30}
         paddingBottom={10}
