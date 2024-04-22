a18483c00a9b833a6fe126600243ac0107da12b9
wambui gitau <ywambui10@gmail.com>
2024-04-22 11:24:06 +0300

    issue with redirecting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    outputStandalone: true,
+  },
 }
+
+
 
 module.exports = nextConfig
