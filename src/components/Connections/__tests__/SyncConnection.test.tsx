e71ce86d00b3e42e93b51467a1349280e22186f0
mdshamoon <mohdshamoon841@gmail.com>
2024-04-22 23:55:55 +0530

    fixed all test cases

--- src/components/Connections/__tests__/SyncConnection.test.tsx
+++ src/components/Connections/__tests__/SyncConnection.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within, act, waitFor } from '@testing-library/react';
+import { render, screen, within, waitFor } from '@testing-library/react';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import '@testing-library/jest-dom';
