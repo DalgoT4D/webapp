54b20d11e7a3d24e11a17b772e3509b058e8907e
Kurund Jalmi <kurund.jalmi@gmail.com>
2024-04-26 19:27:25 +0100

    deepscan fixes

--- src/components/DBT/__tests__/CreateWorkspace.test.tsx
+++ src/components/DBT/__tests__/CreateWorkspace.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
