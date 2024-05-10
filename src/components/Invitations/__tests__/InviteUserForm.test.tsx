a15510f4ef16cf288a88e1c7b6b6fbf6bf55b6a1
mdshamoon <mohdshamoon841@gmail.com>
2024-05-10 11:55:18 +0530

    fixed test case

--- src/components/Invitations/__tests__/InviteUserForm.test.tsx
+++ src/components/Invitations/__tests__/InviteUserForm.test.tsx
@@ -1,11 +1,23 @@
-import { act, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import '@testing-library/jest-dom';
 import InviteUserForm from '../InviteUserForm';
 import userEvent from '@testing-library/user-event';
+import { SWRConfig } from 'swr';
 
-// const user = userEvent.setup();
+const roles = [
+  {
+    uuid: 'fake-uuid-1',
+    slug: 'account-manager',
+    name: 'Account manager',
+  },
+  {
+    uuid: 'fake-uuid-2',
+    slug: 'pipeline-manager',
+    name: 'Pipeline manager',
+  },
+];
 
 const pushMock = jest.fn();
 
@@ -26,28 +38,51 @@ describe('Invite user', () => {
     user: { email: 'a' },
   };
 
-  it('initial render of the invite user form - success', async () => {
-    await act(() =>
-      render(
-        <SessionProvider session={mockSession}>
-          <InviteUserForm
-            mutate={mutate}
-            showForm={true}
-            setShowForm={setShowForm}
-          />
-        </SessionProvider>
-      )
-    );
+  const mockRoles = jest.fn().mockResolvedValue({
+    ok: true,
+    json: jest.fn().mockResolvedValue(roles),
+  });
 
+  const inviteForm = (
+    <SWRConfig
+      value={{
+        dedupingInterval: 0,
+        fetcher: (resource) =>
+          mockRoles(resource, {}).then((res: any) => res.json()),
+        provider: () => new Map(),
+      }}
+    >
+      <SessionProvider session={mockSession}>
+        <InviteUserForm
+          mutate={mutate}
+          showForm={true}
+          setShowForm={setShowForm}
+        />
+      </SessionProvider>
+    </SWRConfig>
+  );
+
+  const user = userEvent.setup();
+  it('initial render of the invite user form - success', async () => {
     const inviteUserApiMock = jest.fn().mockResolvedValueOnce({
       ok: true,
       json: jest.fn().mockResolvedValueOnce({ success: 1 }),
     });
-
     (global as any).fetch = inviteUserApiMock;
 
+    render(inviteForm);
+
     const emailInput = screen.getByLabelText('Email*');
-    await userEvent.type(emailInput, 'inviteuser@gmail.com');
+    await user.type(emailInput, 'inviteuser@gmail.com');
+
+    const dropdown = screen.getByRole('combobox');
+    await user.click(dropdown);
+
+    const firstOption = screen.getAllByRole('option')[0];
+    expect(firstOption).toBeVisible();
+
+    // Select an option
+    await user.click(firstOption);
 
     const savebutton = screen.getByTestId('savebutton');
     await userEvent.click(savebutton);
@@ -57,18 +92,6 @@ describe('Invite user', () => {
   });
 
   it('initial render of the invite user form - failure', async () => {
-    await act(() =>
-      render(
-        <SessionProvider session={mockSession}>
-          <InviteUserForm
-            mutate={mutate}
-            showForm={true}
-            setShowForm={setShowForm}
-          />
-        </SessionProvider>
-      )
-    );
-
     const inviteUserApiMock = jest.fn().mockResolvedValueOnce({
       ok: false,
       json: jest.fn().mockResolvedValueOnce({ detail: 'something went wrong' }),
@@ -76,8 +99,19 @@ describe('Invite user', () => {
 
     (global as any).fetch = inviteUserApiMock;
 
+    render(inviteForm);
+
     const emailInput = screen.getByLabelText('Email*');
     await userEvent.type(emailInput, 'inviteuser@gmail.com');
+
+    const dropdown = screen.getByRole('combobox');
+    await user.click(dropdown);
+
+    const firstOption = screen.getAllByRole('option')[0];
+    expect(firstOption).toBeVisible();
+
+    // Select an option
+    await user.click(firstOption);
 
     const savebutton = screen.getByTestId('savebutton');
     await userEvent.click(savebutton);
