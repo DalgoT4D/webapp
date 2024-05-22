e909d7b3bbb87d80480ecfea00938682ceeb4a94
Abhishek-N <abhisheknshetty97@gmail.com>
2024-05-22 20:43:06 +0530

    pending actions accordion component

---
+++ src/components/Connections/PendingActions.tsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+  Button,
+  Box,
+} from '@mui/material';
+import connectionIcon from '@/assets/icons/connection.svg';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { httpGet } from '@/helpers/http'; // Import your HTTP helper function
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+
+const PendingActionsAccordion = () => {
+  const [schemaChangeData, setSchemaChangeData] = useState<any[]>([]);
+  const [connectionNameMap, setConnectionNameMap] = useState<
+    Record<string, string>
+  >({}); // Mapping of connection_id to connection_name
+  const { data: session }: any = useSession();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch schema change data from the API
+        const schemaChangeResponse = await httpGet(
+          session,
+          'airbyte/v1/connection/schema_change'
+        );
+        setSchemaChangeData(schemaChangeResponse);
+
+        // Fetch connection data to build the connection name map
+        const connectionDataResponse = await httpGet(
+          session,
+          'airbyte/v1/connections'
+        );
+        const connectionMap: Record<string, string> = {};
+        connectionDataResponse.forEach((connection: any) => {
+          connectionMap[connection.connectionId] = connection.name;
+        });
+        setConnectionNameMap(connectionMap);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [session]);
+
+  if (!schemaChangeData || schemaChangeData.length === 0) {
+    return null;
+  }
+
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography variant="h6" fontWeight={800}>
+          Pending Actions
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box sx={{ width: '100%' }}>
+          {schemaChangeData.map((schemaChange: any, index: number) => {
+            const connectionId = schemaChange.connection_id;
+            const connectionName = connectionNameMap[connectionId];
+            const schemaChangeType = schemaChange.change_type;
+
+            // Define the styles for the label based on the schema change type
+            const labelStyles = {
+              color: schemaChangeType === 'breaking' ? 'white' : '#D35D5D',
+              backgroundColor:
+                schemaChangeType === 'breaking' ? '#D35D5D' : 'transparent',
+              border: `1px solid #D35D5D`,
+              borderRadius: '3px',
+              padding: '2px 6px',
+              fontSize: '0.6rem',
+              fontWeight: 'bold',
+            };
+
+            return (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  marginBottom: '20px',
+                }}
+              >
+                {/* Connection Name */}
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Image
+                    style={{ marginRight: 10 }}
+                    src={connectionIcon}
+                    alt="dbt icon"
+                  />
+                  <Typography variant="body1" fontWeight={800}>
+                    {connectionName} &nbsp;&nbsp;&nbsp;
+                    {schemaChangeType && (
+                      <Typography component="span" sx={labelStyles}>
+                        {schemaChangeType === 'breaking'
+                          ? 'Breaking'
+                          : 'Updates'}
+                      </Typography>
+                    )}
+                  </Typography>
+                </Box>
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: 'bold' }}
+                ></Typography>
+                <Button variant="outlined">View</Button>
+              </Box>
+            );
+          })}
+        </Box>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default PendingActionsAccordion;
