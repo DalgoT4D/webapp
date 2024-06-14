77673f82cf48de4c6f0941ad2e8e21f3d435c3f0
mdshamoon <mohdshamoon841@gmail.com>
2024-06-14 16:52:27 +0530

    fixed feedback

--- src/config/menu.tsx
+++ src/config/menu.tsx
@@ -4,6 +4,7 @@ import IngestIcon from '@/assets/icons/ingest';
 import TransformIcon from '@/assets/icons/transform';
 import PipelineIcon from '@/assets/icons/pipeline';
 import OrchestrateIcon from '@/assets/icons/orchestrate';
+import DataQualityIcon from '@/assets/icons/dataQuality';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 import { primaryColor } from './theme';
@@ -42,15 +43,6 @@ export const sideMenu: MenuOption[] = [
     className: 'usage_walkthrough',
   },
   {
-    index: 1.2,
-    title: 'Data Quality',
-    path: '/elementary',
-    icon: (selected: boolean) => <UsageIcon fill={getColor(selected)} />,
-    parent: 1,
-    className: 'usage_walkthrough',
-    hide: !showElementaryMenu,
-  },
-  {
     index: 2,
     title: 'Pipeline overview',
     path: '/pipeline',
@@ -83,9 +75,16 @@ export const sideMenu: MenuOption[] = [
     parent: 2,
     className: 'orchestrate_walkthrough',
   },
-
   {
     index: 3,
+    title: 'Data Quality',
+    path: '/elementary',
+    icon: (selected: boolean) => <DataQualityIcon fill={getColor(selected)} />,
+    className: 'data_quality_walkthrough',
+    hide: !showElementaryMenu,
+  },
+  {
+    index: 4,
     title: 'User management',
     path: '/user-management',
     icon: () => <SupervisorAccountIcon />,
