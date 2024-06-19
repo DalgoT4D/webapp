84992c7e283e47957fc0c0b212ce2e46696b02d5
mdshamoon <mohdshamoon841@gmail.com>
2024-06-19 13:17:06 +0530

    multiple source type fix

--- src/components/Sources/SourceConfigInput.tsx
+++ src/components/Sources/SourceConfigInput.tsx
@@ -8,7 +8,6 @@ import Input from '../UI/Input/Input';
 import ConnectorConfigInput from '@/helpers/ConnectorConfigInput';
 
 export interface SourceConfigInputprops {
-  errors: any;
   specs: Array<any>;
   registerFormFieldValue: (...args: any) => any;
   control: any;
@@ -34,9 +33,7 @@ export type SourceSpec = {
 };
 
 export const SourceConfigInput = ({
-  errors,
   specs,
-  registerFormFieldValue,
   control,
   setFormValue,
   source,
@@ -88,72 +85,72 @@ export const SourceConfigInput = ({
       {connectorSpecs
         ?.sort((input1, input2) => input1.order - input2.order)
         .map((spec: SourceSpec, idx: number) => {
-          const [parent, field] = spec.field.split('.');
-          let hasError = false;
-          let errorMessge = '';
-          if (parent && field) {
-            hasError = errors && errors.config && !!errors[parent][field];
-            errorMessge =
-              errors &&
-              errors.config &&
-              (errors[parent][field]?.message as string);
-          }
           return spec.type === 'string' ? (
             spec.airbyte_secret ? (
               <React.Fragment key={idx}>
-                <Input
-                  error={hasError}
-                  helperText={errorMessge}
-                  sx={{ width: '100%' }}
-                  label={spec.title}
-                  register={registerFormFieldValue}
+                <Controller
                   name={spec.field}
-                  variant="outlined"
-                  type={showPasswords[`${spec.field}`] ? 'text' : 'password'}
-                  required={spec.required}
-                  defaultValue={spec.default}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        {!source ? (
-                          <Box>
-                            <IconButton
-                              onClick={() =>
-                                handleClickShowPassword(`${spec.field}`)
-                              }
-                              edge="end"
-                            >
-                              {showPasswords[`${spec.field}`] ? (
-                                <VisibilityOutlinedIcon />
-                              ) : (
-                                <VisibilityOffOutlinedIcon />
-                              )}
-                            </IconButton>
-                          </Box>
-                        ) : (
-                          <></>
-                        )}
-                      </InputAdornment>
-                    ),
-                  }}
-                ></Input>
+                  control={control}
+                  rules={{ required: spec.required && 'Required' }}
+                  render={({ field: { ref, ...rest }, fieldState }) => (
+                    <Input
+                      {...rest}
+                      error={!!fieldState.error}
+                      helperText={fieldState.error?.message}
+                      sx={{ width: '100%' }}
+                      label={`${spec.title}${spec.required ? '*' : ''}`}
+                      variant="outlined"
+                      type={
+                        showPasswords[`${spec.field}`] ? 'text' : 'password'
+                      }
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            {!source ? (
+                              <Box>
+                                <IconButton
+                                  onClick={() =>
+                                    handleClickShowPassword(`${spec.field}`)
+                                  }
+                                  edge="end"
+                                >
+                                  {showPasswords[`${spec.field}`] ? (
+                                    <VisibilityOutlinedIcon />
+                                  ) : (
+                                    <VisibilityOffOutlinedIcon />
+                                  )}
+                                </IconButton>
+                              </Box>
+                            ) : (
+                              <></>
+                            )}
+                          </InputAdornment>
+                        ),
+                      }}
+                    ></Input>
+                  )}
+                />
                 <Box sx={{ m: 2 }} />
               </React.Fragment>
             ) : (
               <React.Fragment key={idx}>
-                <Input
-                  error={hasError}
-                  helperText={errorMessge}
-                  sx={{ width: '100%' }}
-                  label={spec.title}
-                  variant="outlined"
-                  register={registerFormFieldValue}
+                <Controller
                   name={spec.field}
-                  required={spec.required}
-                  defaultValue={spec.default}
-                  disabled={false}
-                  inputProps={{ pattern: spec.pattern }}
-                ></Input>
+                  control={control}
+                  rules={{ required: spec.required && 'Required' }}
+                  render={({ field: { ref, ...rest }, fieldState }) => (
+                    <Input
+                      {...rest}
+                      error={!!fieldState.error}
+                      helperText={fieldState.error?.message}
+                      sx={{ width: '100%' }}
+                      label={`${spec.title}${spec.required ? '*' : ''}`}
+                      variant="outlined"
+                      disabled={false}
+                      inputProps={{ pattern: spec.pattern }}
+                    ></Input>
+                  )}
+                />
                 <Box sx={{ m: 2 }} />
               </React.Fragment>
             )
@@ -162,7 +159,7 @@ export const SourceConfigInput = ({
               <Controller
                 name={spec.field}
                 control={control}
-                rules={{ required: spec.required }}
+                rules={{ required: spec.required && 'Required' }}
                 render={({ field: { value } }) => (
                   <MultiTagInput
                     disabled={false}
@@ -177,19 +174,23 @@ export const SourceConfigInput = ({
             </React.Fragment>
           ) : spec.type === 'integer' ? (
             <React.Fragment key={idx}>
-              <Input
-                error={hasError}
-                helperText={errorMessge}
-                disabled={false}
-                sx={{ width: '100%' }}
-                label={spec.title}
-                variant="outlined"
-                register={registerFormFieldValue}
+              <Controller
                 name={spec.field}
-                required={spec.required}
-                defaultValue={spec.default}
-                type="number"
-              ></Input>
+                control={control}
+                rules={{ required: spec.required && 'Required' }}
+                render={({ field: { ref, ...rest }, fieldState }) => (
+                  <Input
+                    {...rest}
+                    error={!!fieldState.error}
+                    helperText={fieldState.error?.message}
+                    disabled={false}
+                    sx={{ width: '100%' }}
+                    label={`${spec.title}${spec.required ? '*' : ''}`}
+                    variant="outlined"
+                    type="number"
+                  ></Input>
+                )}
+              />
               <Box sx={{ m: 2 }} />
             </React.Fragment>
           ) : spec.type === 'object' ? (
@@ -197,8 +198,8 @@ export const SourceConfigInput = ({
               <Controller
                 name={spec.field}
                 control={control}
-                rules={{ required: spec.required }}
-                render={({ field }) => (
+                rules={{ required: spec.required && 'Required' }}
+                render={({ field, fieldState }) => (
                   <Autocomplete
                     disabled={source ? true : false}
                     data-testid="autocomplete"
@@ -211,11 +212,11 @@ export const SourceConfigInput = ({
                     renderInput={(params) => (
                       <Input
                         name={spec.field}
-                        error={hasError}
-                        helperText={errorMessge}
+                        error={!!fieldState.error}
+                        helperText={fieldState.error?.message}
                         {...params}
                         variant="outlined"
-                        label={spec.title}
+                        label={`${spec.title}${spec.required ? '*' : ''}`}
                       />
                     )}
                   />
