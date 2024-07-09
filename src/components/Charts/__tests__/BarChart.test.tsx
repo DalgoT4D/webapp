255793616e2fa8092fe04aa3560ba74448381101
himanshudube97 <himanshu.dube13@gmail.com>
2024-07-09 22:40:53 +0530

    added test for the barchart

---
+++ src/components/Charts/__tests__/BarChart.test.tsx
@@ -0,0 +1,50 @@
+import * as d3 from 'd3';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { BarChart } from '../BarChart';
+
+const mockData = [
+  {
+    label: 'test1',
+    value: 40,
+    barTopLabel: 'top 40',
+  },
+  {
+    label: 'test2',
+    value: 70,
+    barTopLabel: 'top70',
+  },
+];
+
+describe('BarChart', () => {
+  beforeEach(() => {
+    render(<BarChart data={mockData} />);
+  });
+
+  it('should render the svg element', () => {
+    const svgElement = screen.getByRole('img');
+    expect(svgElement).toBeInTheDocument();
+  });
+
+  it('shows tooltip on label hover', () => {
+    mockData.forEach(async (data) => {
+      const label = screen.getByText(data.label);
+      fireEvent.mouseOver(label);
+      const tooltip = screen.getByText(data.label);
+      expect(tooltip).toBeInTheDocument();
+      await waitFor(() => {
+        const tooltip = screen.getByText(data.label);
+        expect(tooltip).toBeInTheDocument();
+        expect(tooltip).toHaveStyle('opacity: 0.9');
+      });
+
+      fireEvent.mouseLeave(label);
+
+      await waitFor(() => {
+        const tooltip = screen.getByText(data.label);
+        expect(tooltip).toHaveStyle('opacity: 0');
+      });
+      //   fireEvent.mouseLeave(label);
+      //   expect(tooltip).not.toBeInTheDocument();
+    });
+  });
+});
