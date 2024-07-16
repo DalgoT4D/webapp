629c900ae47e0ce977e532b41ad6efdf4c77b934
himanshudube97 <himanshu.dube13@gmail.com>
2024-07-16 16:04:54 +0530

    issue resoved

--- src/components/Charts/__tests__/DateTimeInsights.test.tsx
+++ src/components/Charts/__tests__/DateTimeInsights.test.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { DateTimeInsights } from '../DateTimeInsights';
 
-
-
 // Mock dependencies
 jest.mock('next-auth/react', () => ({
-  useSession: jest.fn().mockReturnValue({ data: { user: { name: 'Test User' } } }),
+  useSession: jest
+    .fn()
+    .mockReturnValue({ data: { user: { name: 'Test User' } } }),
 }));
-jest.mock('next/image', () => ({ src, alt }: { src: string; alt: string }) => <img src={src} alt={alt} />);
-// jest.mock('fetch');
 
 const mockBarProps = {
   data: [
@@ -23,20 +21,15 @@ const mockPostBody = {};
 describe('DateTimeInsights', () => {
   it('renders the component with chart type', () => {
     render(
-      // <SessionProvider session={null}>
-        <DateTimeInsights
-          minDate="2021-01-01"
-          maxDate="2022-01-01"
-          barProps={mockBarProps}
-          type="chart"
-          postBody={mockPostBody}
-        />
-      // </SessionProvider>
+      <DateTimeInsights
+        minDate="2021-01-01"
+        maxDate="2022-01-01"
+        barProps={mockBarProps}
+        type="chart"
+        postBody={mockPostBody}
+      />
     );
 
     expect(screen.getByRole('outerbox')).toBeInTheDocument();
-    // expect(screen.getByText('year')).toBeInTheDocument();
-    // expect(screen.getByAltText('switch icon')).toBeInTheDocument();
   });
-
 });
