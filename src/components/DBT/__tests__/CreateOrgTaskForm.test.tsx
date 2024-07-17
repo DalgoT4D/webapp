edb695105676adf000caf2f7500b1a0d3462db64
himanshudube97 <himanshu.dube13@gmail.com>
2024-07-17 15:04:40 +0530

    commit

--- src/components/DBT/__tests__/CreateOrgTaskForm.test.tsx
+++ src/components/DBT/__tests__/CreateOrgTaskForm.test.tsx
@@ -5,6 +5,7 @@ import {
   fireEvent,
   waitFor,
   within,
+  getByTestId,
 } from '@testing-library/react';
 
 import CreateOrgTaskForm from '../CreateOrgTaskForm';
@@ -69,7 +70,7 @@ describe('CreateOrgTaskForm', () => {
     expect(screen.getByTestId('optionsList')).toBeInTheDocument();
     expect(screen.getByTestId('taskList')).toBeInTheDocument();
     expect(screen.getByText(/Save/i)).toBeInTheDocument();
-    expect(screen.getByText(/Cancel/i)).toBeInTheDocument();
+    expect(screen.getByTestId('closebutton')).toBeInTheDocument();
   });
 
   it('fetches and displays tasks correctly', async () => {
@@ -81,7 +82,7 @@ describe('CreateOrgTaskForm', () => {
       Promise.resolve({
         json: () => Promise.resolve(mockTasks),
       })
-      
+
     ) as jest.Mock;
 
     setup();
@@ -109,101 +110,99 @@ describe('CreateOrgTaskForm', () => {
     //    expect(screen.getByRole("presentation")).toHaveClass("base-Popper-root");
   });
 
-  //   it('submits the form with correct data', async () => {
-  //     const mockConfig = {
-  //       flags: ['flag1', 'flag2'],
-  //       options: ['option1', 'option2'],
-  //     };
-  //     global.fetch = jest.fn((url) => {
-  //       if (url.endsWith('/tasks/')) {
-  //         return Promise.resolve({
-  //           json: () =>
-  //             Promise.resolve([
-  //               { id: 'task-1', label: 'Task 1' },
-  //               { id: 'task-2', label: 'Task 2' },
-  //             ]),
-  //         });
-  //       }
-  //       if (url.includes('/tasks/task-1/config/')) {
-  //         return Promise.resolve({
-  //           json: () => Promise.resolve(mockConfig),
-  //         });
-  //       }
-  //       if (url.endsWith('/prefect/tasks/')) {
-  //         return Promise.resolve({
-  //           json: () => Promise.resolve({}),
-  //         });
-  //       }
-  //       return Promise.reject(new Error('not found'));
-  //     }) as jest.Mock;
+  it('submits the form with correct data', async () => {
+    const mockConfig = {
+      flags: ['flag1', 'flag2'],
+      options: ['option1', 'option2'],
+    };
+    global.fetch = jest.fn((url) => {
+      if (url.endsWith('/tasks/')) {
+        return Promise.resolve({
+          json: () =>
+            Promise.resolve([
+              { id: 'task-1', label: 'Task 1' },
+              { id: 'task-2', label: 'Task 2' },
+            ]),
+        });
+      }
+      if (url.includes('/tasks/task-1/config/')) {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockConfig),
+        });
+      }
+      if (url.endsWith('/prefect/tasks/')) {
+        return Promise.resolve({
+          json: () => Promise.resolve({}),
+        });
+      }
+      return Promise.reject(new Error('not found'));
+    }) as jest.Mock;
 
-  //     setup();
+    setup();
 
-  //     // Select a task
-  //     fireEvent.click(screen.getByLabelText(/Select task/i));
-  //     await waitFor(() => screen.getByText('Task 1'));
-  //     fireEvent.click(screen.getByText('Task 1'));
+    // Select a task
+    fireEvent.click(screen.getByLabelText(/Select task/i));
+    await waitFor(() => screen.getByText('Task 1'));
+    fireEvent.click(screen.getByText('Task 1'));
 
-  //     // Select a flag
-  //     fireEvent.click(screen.getByLabelText(/Flags/i));
-  //     await waitFor(() => screen.getByText('flag1'));
-  //     fireEvent.click(screen.getByText('flag1'));
+    // Select a flag
+    fireEvent.click(screen.getByLabelText(/Flags/i));
+    await waitFor(() => screen.getByText('flag1'));
+    fireEvent.click(screen.getByText('flag1'));
 
-  //     // Add an option
-  //     fireEvent.click(screen.getByText(/Add/i));
-  //     fireEvent.change(screen.getByLabelText(/Options/i), {
-  //       target: { value: 'option1' },
-  //     });
+    // Add an option
+    fireEvent.click(screen.getByText(/Add/i));
+    fireEvent.change(screen.getByLabelText(/Options/i), {
+      target: { value: 'option1' },
+    });
 
-  //     // Submit the form
-  //     fireEvent.click(screen.getByText(/Save/i));
+    // Submit the form
+    fireEvent.click(screen.getByText(/Save/i));
 
-  //     await waitFor(() => {
-  //       expect(global.fetch).toHaveBeenCalledWith(
-  //         expect.stringContaining('/prefect/tasks/'),
-  //         expect.objectContaining({
-  //           method: 'POST',
-  //           body: JSON.stringify({
-  //             task_slug: 'task-1',
-  //             flags: ['flag1'],
-  //             options: { option1: '' },
-  //           }),
-  //         })
-  //       );
-  //     });
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledWith(
+        expect.stringContaining('/prefect/tasks/'),
+        expect.objectContaining({
+          method: 'POST',
+          body: JSON.stringify({
+            task_slug: 'task-1',
+            flags: ['flag1'],
+            options: { option1: '' },
+          }),
+        })
+      );
+    });
 
-  //     expect(successToast).toHaveBeenCalledWith(
-  //       'Org Task created successfully',
-  //       [],
-  //       mockGlobalContext
-  //     );
-  //   });
+    expect(successToast).toHaveBeenCalledWith(
+      'Org Task created successfully',
+      [],
+      mockGlobalContext
+    );
+  });
 
-  //   it('handles form submission errors', async () => {
-  //     global.fetch = jest.fn(() =>
-  //       Promise.reject(new Error('Submission error'))
-  //     ) as jest.Mock;
+  it('handles form submission errors', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.reject(new Error('Submission error'))
+    ) as jest.Mock;
 
-  //     setup();
+    setup();
 
-  //     // Submit the form without any data
-  //     fireEvent.click(screen.getByText(/Save/i));
+    // Submit the form without any data
+    fireEvent.click(screen.getByText(/Save/i));
 
-  //     await waitFor(() => {
-  //       expect(errorToast).toHaveBeenCalledWith(
-  //         'Submission error',
-  //         [],
-  //         mockGlobalContext
-  //       );
-  //     });
-  //   });
+    await waitFor(() => {
+      expect(errorToast).toHaveBeenCalledWith(
+        'Submission error',
+        [],
+        mockGlobalContext
+      );
+    });
+  });
 
-  //   it('handles form close correctly', () => {
-  //     const { getByText } = setup();
-
-  //     // Close the form
-  //     fireEvent.click(getByText(/Cancel/i));
-
-  //     expect(mockGlobalContext.dispatch).toHaveBeenCalled();
-  //   });
+  // it('handles form close correctly', () => {
+  //   setup();
+  //   fireEvent.click(screen.getByTestId("closebutton"));
+  //   // expect(screen.getByTestId("taskform")).not.toBeInTheDocument();
+  //   // expect(mockGlobalContext.dispatch).toHaveBeenCalled();
+  // });
 });
