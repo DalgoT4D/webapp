175528e9b5f3952934dc1c6ab2c46c6e5437f8d0
himanshudube97 <himanshu.dube13@gmail.com>
2024-07-23 19:01:28 +0530

    added the overwrite component

--- src/components/UI/FullScreenLoader/FullScreenLoader.tsx
+++ src/components/UI/FullScreenLoader/FullScreenLoader.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
-export const FullPageLoader: React.FC = () => {
+interface FullPageBackgroundProps {
+  children: React.ReactNode;
+}
+
+export const FullPageBackground: React.FC<FullPageBackgroundProps> = ({ children }) => {
   return (
     <Box
       sx={{
-        
         position: 'fixed',
         top: 0,
         left: 0,
@@ -13,17 +16,13 @@ export const FullPageLoader: React.FC = () => {
         height: '100vh',
         backgroundColor: 'rgba(0, 0, 0, 0.8)',
         display: 'flex',
-        gap: "1rem",
-        flexDirection: "column",
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         zIndex: 1300,
       }}
     >
-      <CircularProgress sx={{color: "#FFFFFF"}}/>
-      <Typography sx={{ fontWeight: '600', fontSize: '20px', color: "#FFFFFF" }}>
-        Prepping your data output...
-      </Typography>
+      {children}
     </Box>
   );
 };
