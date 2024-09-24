6b27ecd30d89a5135b15d21c0ef7e420f042a3bb
himanshudube97 <himanshu.dube13@gmail.com>
2024-09-24 14:58:47 +0530

    fixes done

--- src/assets/icons/aianalysis.tsx
+++ src/assets/icons/aianalysis.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 const AiAnalysis = (props: any) => (
-<svg
+  <svg
     width="26"
     height="20"
     viewBox="0 0 26 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
->
+  >
     <path
       d="M10.0598 17.9178H3.66699C2.56242 17.9178 1.66699 17.0224 1.66699 15.9178V3.75C1.66699 2.64543 2.56242 1.75 3.66699 1.75H12.9556C14.0602 1.75 14.9556 2.64543 14.9556 3.75V9.10913"
-      stroke="#2C3E57"
+      stroke={props.fill || '#2C3E57'}
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
-      fill={props.fill || '#7D8998'}
+      fill="white"
     />
     <path
       d="M4.10352 4.85156H12.2981"
-      stroke="#2C3E57"
+      stroke={props.fill || '#2C3E57'}
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -26,7 +26,7 @@ const AiAnalysis = (props: any) => (
     />
     <path
       d="M4.1037 8.17383H12.2983"
-      stroke="#2C3E57"
+      stroke={props.fill || '#2C3E57'}
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -34,7 +34,7 @@ const AiAnalysis = (props: any) => (
     />
     <path
       d="M4.10254 11.4941H7.42469"
-      stroke="#2C3E57"
+      stroke={props.fill || '#2C3E57'}
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -42,7 +42,7 @@ const AiAnalysis = (props: any) => (
     />
     <path
       d="M4.1037 14.5957H7.42585"
-      stroke="#2C3E57"
+      stroke={props.fill || '#2C3E57'}
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -50,7 +50,7 @@ const AiAnalysis = (props: any) => (
     />
     <path
       d="M16.0002 19.5H16.0344C18.2555 19.5 19.9654 19.5 21.2586 19.3563C22.5547 19.2123 23.5424 18.915 24.2288 18.2286C24.9152 17.5421 25.2125 16.5545 25.3566 15.2583C25.5003 13.9651 25.5002 12.2553 25.5002 10.0341V10V9.96586C25.5002 7.74471 25.5003 6.03487 25.3566 4.74166C25.2125 3.44552 24.9152 2.45786 24.2288 1.77145C23.5424 1.08503 22.5547 0.787699 21.2586 0.643683C19.9654 0.499993 18.2555 0.499996 16.0344 0.5H16.0002H15.9661C13.745 0.499996 12.0351 0.499993 10.7419 0.643683C9.44577 0.787699 8.4581 1.08503 7.77169 1.77145C7.08528 2.45786 6.78794 3.44552 6.64393 4.74166C6.50024 6.03487 6.50024 7.74471 6.50024 9.96587V10V10.0341C6.50024 12.2553 6.50024 13.9651 6.64393 15.2583C6.78794 16.5545 7.08528 17.5421 7.77169 18.2286C8.4581 18.915 9.44577 19.2123 10.7419 19.3563C12.0351 19.5 13.745 19.5 15.9661 19.5H16.0002Z"
-      fill="#2C3E57"
+      fill={props.fill || '#2C3E57'}
       stroke="#F4F4F4"
     />
     <path
