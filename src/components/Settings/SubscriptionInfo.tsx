4825a962c2b431eabcf08dc88724d8d93225d7f5
himanshudube97 <himanshu.dube13@gmail.com>
2024-11-19 20:57:37 +0530

    fixed the loader overlapp issue

--- src/components/Settings/SubscriptionInfo.tsx
+++ src/components/Settings/SubscriptionInfo.tsx
@@ -85,7 +85,6 @@ export const SubscriptionInfo = () => {
     }
   }, [session]);
 
-  if (loader) return <CircularProgress />;
   return (
     <>
       <Box
@@ -94,156 +93,169 @@ export const SubscriptionInfo = () => {
           borderRadius: '12px',
           pt: '.5rem',
           pb: '8px',
+          minHeight: '300px',
         }}
       >
-        {orgPlan.features && (
-          <>
-            {/* Plan Details Section */}
-            <Box sx={{ margin: '24px 32px' }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                {/* Plan Information */}
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography
-                    sx={{ fontWeight: 700, color: 'rgba(0, 137, 123, 1)', fontSize: '28px' }}
-                  >
-                    {orgPlan.base_plan}&nbsp;
-                  </Typography>
-                  {orgPlan.superset_included ? (
+        {loader ? (
+          <CircularProgress />
+        ) : (
+          orgPlan.features && (
+            <>
+              {/* Plan Details Section */}
+              <Box sx={{ margin: '24px 32px' }}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                  }}
+                >
+                  {/* Plan Information */}
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     <Typography
-                      sx={{ fontWeight: 700, color: 'rgba(15, 36, 64, 1)', fontSize: '28px' }}
+                      sx={{ fontWeight: 700, color: 'rgba(0, 137, 123, 1)', fontSize: '28px' }}
                     >
-                      {' '}
-                      + Superset
+                      {orgPlan.base_plan}&nbsp;
                     </Typography>
-                  ) : (
-                    ''
-                  )}
-                  <Typography
-                    sx={{
-                      backgroundColor: 'rgba(15, 36, 64, 0.16)',
-                      fontWeight: 700,
-                      fontSize: '12px',
-                      color: 'rgba(15, 36, 64, 0.87)',
-                      ml: '1rem',
-                      padding: '3px 13px',
-                      borderRadius: '4px',
-                    }}
+                    {orgPlan.superset_included ? (
+                      <Typography
+                        sx={{ fontWeight: 700, color: 'rgba(15, 36, 64, 1)', fontSize: '28px' }}
+                      >
+                        + Superset
+                      </Typography>
+                    ) : (
+                      ''
+                    )}
+                    <Typography
+                      sx={{
+                        backgroundColor: 'rgba(15, 36, 64, 0.16)',
+                        fontWeight: 700,
+                        fontSize: '12px',
+                        color: 'rgba(15, 36, 64, 0.87)',
+                        ml: '1rem',
+                        padding: '3px 13px',
+                        borderRadius: '4px',
+                      }}
+                    >
+                      {orgPlan.subscription_duration}
+                    </Typography>
+                  </Box>
+
+                  <Button
+                    variant="contained"
+                    disabled={
+                      !(
+                        orgPlan.can_upgrade_plan &&
+                        permissions?.includes('can_initiate_org_plan_upgrade')
+                      )
+                    }
+                    sx={{ p: '8px 24px' }}
+                    onClick={hanldeUpgradePlan}
                   >
-                    {orgPlan.subscription_duration}
-                  </Typography>
+                    Upgrade
+                  </Button>
                 </Box>
 
-                <Button
-                  variant="contained"
-                  disabled={
-                    !(
-                      orgPlan.can_upgrade_plan &&
-                      permissions?.includes('can_initiate_org_plan_upgrade')
-                    )
-                  }
-                  sx={{ p: '8px 24px' }}
-                  onClick={hanldeUpgradePlan}
-                >
-                  Upgrade
-                </Button>
-              </Box>
+                {/* Features Section */}
+                <Box minHeight="153px">
+                  {/* Pipeline Features */}
+                  <List disablePadding>
+                    <ListItem disablePadding sx={{ pt: '8px' }}>
+                      <CheckCircleOutlineIcon sx={{ color: '#00897B', fontSize: '20px' }} />
+                      <Typography
+                        sx={{ ml: 1, fontSize: '15px', fontWeight: 600, color: '#0F2440AD' }}
+                      >
+                        {orgPlan.features?.pipeline.join(' | ')}
+                      </Typography>
+                    </ListItem>
 
-              {/* Features Section */}
-              <Box minHeight="153px">
-                {/* Pipeline Features */}
-                <List disablePadding>
-                  <ListItem disablePadding sx={{ pt: '8px' }}>
-                    <CheckCircleOutlineIcon sx={{ color: '#00897B', fontSize: '20px' }} />
-                    <Typography
-                      sx={{ ml: 1, fontSize: '15px', fontWeight: 600, color: '#0F2440AD' }}
-                    >
-                      {orgPlan.features?.pipeline.join(' | ')}
-                    </Typography>
-                  </ListItem>
+                    {/* Other Features */}
+                    {Object.keys(orgPlan.features).map((key) => {
+                      if (key === 'pipeline') return null;
 
-                  {/* Other Features */}
-                  {Object.keys(orgPlan.features).map((key) => {
-                    if (key === 'pipeline') return null;
-
-                    const featureItems = orgPlan.features[key];
-                    return (
-                      <Box key={key}>
-                        {Array.isArray(featureItems) ? (
-                          featureItems.map((item, index) => (
-                            <ListItem disablePadding key={index} sx={{ pt: '8px' }}>
-                              <CheckCircleOutlineIcon sx={{ color: '#00897B', fontSize: '20px' }} />
-                              <Typography
-                                sx={{
-                                  ml: 1,
-                                  fontSize: '15px',
-                                  fontWeight: 600,
-                                  color: '#0F2440AD',
-                                }}
-                              >
-                                {item}
-                              </Typography>
+                      const featureItems = orgPlan.features[key];
+                      return (
+                        <Box key={key}>
+                          {Array.isArray(featureItems) ? (
+                            featureItems.map((item, index) => (
+                              <ListItem disablePadding key={index} sx={{ pt: '8px' }}>
+                                <CheckCircleOutlineIcon
+                                  sx={{ color: '#00897B', fontSize: '20px' }}
+                                />
+                                <Typography
+                                  sx={{
+                                    ml: 1,
+                                    fontSize: '15px',
+                                    fontWeight: 600,
+                                    color: '#0F2440AD',
+                                  }}
+                                >
+                                  {item}
+                                </Typography>
+                              </ListItem>
+                            ))
+                          ) : (
+                            <ListItem
+                              disablePadding
+                              sx={{ pt: '8px', fontWeight: 600, color: '#0F2440AD' }}
+                            >
+                              <CheckCircleOutlineIcon />
+                              <Typography sx={{ ml: 1 }}>{featureItems}</Typography>
                             </ListItem>
-                          ))
-                        ) : (
-                          <ListItem
-                            disablePadding
-                            sx={{ pt: '8px', fontWeight: 600, color: '#0F2440AD' }}
-                          >
-                            <CheckCircleOutlineIcon />
-                            <Typography sx={{ ml: 1 }}>{featureItems}</Typography>
-                          </ListItem>
-                        )}
-                      </Box>
-                    );
-                  })}
-                </List>
+                          )}
+                        </Box>
+                      );
+                    })}
+                  </List>
+                </Box>
               </Box>
-            </Box>
-            {/* bottom duration remaining */}
-            <Box
-              display="flex"
-              sx={{
-                backgroundColor: '#EBF6F4',
-                padding: '20px 24px',
-                borderRadius: '8px',
-                margin: '8px 8px 0 8px',
-              }}
-            >
-              <Typography color={'#0F244099'} fontWeight={600}>
-                Start date
-              </Typography>
-              &nbsp;
-              <Typography fontWeight={700} color={'#0F2440'}>
-                {moment(orgPlan.start_date).format('DD MMM, YYYY')}
-              </Typography>
-              &nbsp;-&nbsp;
-              <Typography color={'#0F244099'} fontWeight={600}>
-                End date
-              </Typography>
-              &nbsp;
-              <Typography fontWeight={700} color={'#0F2440'}>
-                {moment(orgPlan.end_date).format('DD MMM, YYYY')}
-              </Typography>
-              &nbsp;-&nbsp;
-              {(() => {
-                const { isExpired, isLessThanAWeek, daysRemaining } = calculatePlanStatus(
-                  orgPlan.end_date
-                );
-                return (
-                  <Typography
-                    fontWeight={600}
-                    color={isExpired ? '#FF0000' : isLessThanAWeek ? '#FF0000' : '#00897B'}
-                  >
-                    {isExpired
-                      ? 'Plan has expired'
-                      : daysRemaining > 0
-                        ? `${daysRemaining} day${daysRemaining > 1 ? 's' : ''} remaining`
-                        : '0 days remaining'}
-                  </Typography>
-                );
-              })()}
-            </Box>
-          </>
+              {/* bottom duration remaining */}
+              <Box
+                display="flex"
+                sx={{
+                  backgroundColor: '#EBF6F4',
+                  padding: '20px 24px',
+                  borderRadius: '8px',
+                  margin: '8px 8px 0 8px',
+                }}
+              >
+                <Typography color={'#0F244099'} fontWeight={600}>
+                  Start date
+                </Typography>
+                &nbsp;
+                <Typography fontWeight={700} color={'#0F2440'}>
+                  {moment(orgPlan.start_date).format('DD MMM, YYYY')}
+                </Typography>
+                &nbsp;-&nbsp;
+                <Typography color={'#0F244099'} fontWeight={600}>
+                  End date
+                </Typography>
+                &nbsp;
+                <Typography fontWeight={700} color={'#0F2440'}>
+                  {moment(orgPlan.end_date).format('DD MMM, YYYY')}
+                </Typography>
+                &nbsp;-&nbsp;
+                {(() => {
+                  const { isExpired, isLessThanAWeek, daysRemaining } = calculatePlanStatus(
+                    orgPlan.end_date
+                  );
+                  return (
+                    <Typography
+                      fontWeight={600}
+                      color={isExpired ? '#FF0000' : isLessThanAWeek ? '#FF0000' : '#00897B'}
+                    >
+                      {isExpired
+                        ? 'Plan has expired'
+                        : daysRemaining > 0
+                          ? `${daysRemaining} day${daysRemaining > 1 ? 's' : ''} remaining`
+                          : '0 days remaining'}
+                    </Typography>
+                  );
+                })()}
+              </Box>
+            </>
+          )
         )}
       </Box>
     </>
