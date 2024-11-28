f69b10a2b4fdc096c24e9dca6a018c84ae7fb5d5
himanshudube97 <himanshu.dube13@gmail.com>
2024-11-28 12:18:18 +0530

    cypress data anlysis test first

--- cypress/e2e/sidemenu/Sidemenu.cy.ts
+++ cypress/e2e/sidemenu/Sidemenu.cy.ts
@@ -6,29 +6,29 @@ describe('Side menu', () => {
   it('Initial render of side menu', () => {
     // parent
     cy.get('[data-testid="side-menu"]').contains('Analysis').should('be.visible');
-    cy.get('[data-testid="side-menu"]').contains('Usage').should('be.visible');
-    cy.get('[data-testid="side-menu"]').contains('Data Analysis').should('be.visible');
+    // cy.get('[data-testid="side-menu"]').contains('Usage').should('be.visible');
+    // cy.get('[data-testid="side-menu"]').contains('Data Analysiss').should('be.visible');
 
     cy.get('[data-testid="side-menu"]').contains('Pipeline overview').should('be.visible');
     // Sub menus for Pipeline overview are by default visible
-    cy.get('[data-testid="side-menu"]').contains('Ingest').should('be.visible');
-    cy.get('[data-testid="side-menu"]').contains('Transform').should('be.visible');
-    cy.get('[data-testid="side-menu"]').contains('Orchestrate').should('be.visible');
+    // cy.get('[data-testid="side-menu"]').contains('Ingest').should('be.visible');
+    // cy.get('[data-testid="side-menu"]').contains('Transform').should('be.visible');
+    // cy.get('[data-testid="side-menu"]').contains('Orchestrate').should('be.visible');
 
     cy.get('[data-testid="side-menu"]').contains('Explore').should('be.visible');
     cy.get('[data-testid="side-menu"]').contains('Data Quality').should('be.visible');
-    cy.get('[data-testid="side-menu"]').contains('User management').should('be.visible');
     cy.get('[data-testid="side-menu"]')
       .contains('Notifications')
       .scrollIntoView()
       .should('be.visible');
+    cy.get('[data-testid="side-menu"]').contains('Settings').should('be.visible');
   });
 
   it('Toggle the side menu collapsible', () => {
     // Collapse the pipeline overview child items
     cy.get('[data-testid="side-menu"]')
       .find('li')
-      .eq(3)
+      .eq(1)
       .then(($li) => {
         console.log('Element at eq(2):', $li); // Log the element to the console
       })
@@ -36,17 +36,16 @@ describe('Side menu', () => {
         cy.get('[data-testid="listButton"]').find('button').click();
       });
 
-    cy.contains('Ingest').should('not.be.visible');
+    cy.contains('Ingest').should('be.visible');
 
-    // Click again to show the child menu items
     cy.get('[data-testid="side-menu"]')
       .find('li')
-      .eq(3)
+      .eq(1)
       .within(() => {
         cy.get('[data-testid="listButton"]').find('button').click();
       });
 
-    cy.contains('Ingest').should('be.visible');
+    cy.contains('Ingest').should('not.be.visible');
   });
 
   it.only('Switching between menu items', () => {
@@ -54,15 +53,22 @@ describe('Side menu', () => {
     cy.get('h4').should('contain', 'Overview');
 
     // Go to user management
-    cy.get('[data-testid="menu-item-5"]').click();
+    cy.get('[data-testid="side-menu"]')
+      .find('li')
+      .eq(5)
+      .within(() => {
+        cy.get('[data-testid="listButton"]').find('button').click();
+      });
+    cy.get('[data-testid="menu-item-5.1"]').click();
     cy.get('[role="tab"][tabindex="0"]').should('contain', 'Users');
+
     cy.get('[role="tab"][tabindex="-1"]').should('contain', 'Pending Invitations');
 
     cy.get('[role="tab"][tabindex="-1"]').should('be.visible').click();
     cy.get('[role="tab"][tabindex="-1"]').should('contain', 'Users');
 
     // Go to ingest
-    cy.get('[data-testid="menu-item-2.1"]').click();
+    cy.get('[data-testid="menu-item-1.1"]').click();
     cy.get('[role="tab"][tabindex="0"]').should('contain', 'Connections');
     cy.get('[role="tab"][tabindex="-1"]').eq(0).should('contain', 'Sources');
     cy.get('[role="tab"][tabindex="-1"]').eq(1).should('contain', 'Your Warehouse');
@@ -74,17 +80,17 @@ describe('Side menu', () => {
     cy.get('[role="tab"][tabindex="0"]').should('contain', 'Your Warehouse');
 
     // // Go to transform
-    cy.get('[data-testid="menu-item-2.2"]').click();
+    cy.get('[data-testid="menu-item-1.2"]').click();
 
     cy.get('h4').should('contain', 'Transform');
     cy.get('[role="tab"][tabindex="0"]').should('contain', 'Setup');
 
     //orchestrate
-    cy.get('[data-testid="menu-item-2.3"]').click();
+    cy.get('[data-testid="menu-item-1.3"]').click();
     cy.get('h4').should('contain', 'Pipelines');
 
     //explore
-    cy.get('[data-testid="menu-item-3"]').click();
+    cy.get('[data-testid="menu-item-2"]').click();
     cy.get('[role="tab"][tabindex="0"]').should('contain', 'Preview');
     cy.get('[role="tab"][tabindex="-1"]').should('contain', 'Data');
   });
