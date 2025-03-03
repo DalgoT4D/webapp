8cb6d653b7324b73af6317d15ee5e7a0abaa3b01
himanshudube97 <himanshu.dube13@gmail.com>
2025-03-03 17:48:42 +0530

    fetching wren based on org

--- src/pages/wren/index.tsx
+++ src/pages/wren/index.tsx
@@ -1,17 +1,52 @@
-import { Box } from '@mui/material';
-import React from 'react';
-
+import { errorToast } from '@/components/ToastMessage/ToastHelper';
+import { GlobalContext } from '@/contexts/ContextProvider';
+import { httpGet } from '@/helpers/http';
+import { Box, Typography } from '@mui/material';
+import { useSession } from 'next-auth/react';
+import React, { useContext, useEffect, useState } from 'react';
 const MyIFrame = () => {
+  const globalContext = useContext(GlobalContext);
+  const { data: session } = useSession();
+  const [wrenUrl, setWrenUrl] = useState('');
+
+  const fetchWrenUrl = async () => {
+    try {
+      const response = await httpGet(session, `organizations/wren`);
+      if (!response.hostname) {
+        errorToast('No hostname found for Wren', [], globalContext);
+      }
+      const { hostname, port } = response;
+      setWrenUrl(`http://${hostname}:${port}/`);
+    } catch (error: any) {
+      errorToast(error.message, [], globalContext);
+      console.error(error, 'error');
+    }
+  };
+  useEffect(() => {
+    if (session) {
+      fetchWrenUrl();
+    }
+  }, [session]);
   return (
     <Box sx={{ width: '100%', height: '100%' }}>
       <div style={{ width: '95%', height: '90vh', border: '1px solid #ddd', margin: '2rem 2rem' }}>
-        <iframe
-          src="https://wren.dalgo.in/modeling"
-          width="100%"
-          height="100%"
-          style={{ border: 'none' }}
-          title="Embedded Page"
-        />
+        {wrenUrl ? (
+          <iframe
+            src={wrenUrl}
+            width="100%"
+            height="100%"
+            style={{ border: 'none' }}
+            title="Embedded Page"
+          />
+        ) : (
+          <Box
+            sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}
+          >
+            <Typography variant="h6" fontSize={'1.5rem'} color="textSecondary">
+              Wren AI has not been setup for this organization
+            </Typography>
+          </Box>
+        )}
       </div>
     </Box>
   );
