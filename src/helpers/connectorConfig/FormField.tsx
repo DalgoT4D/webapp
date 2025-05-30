2e6288f955930cc0439fd3b82df2e13cf28badd4
himanshudube97 <himanshu.dube13@gmail.com>
2025-05-31 02:10:19 +0530

    fix-ca

--- src/helpers/connectorConfig/FormField.tsx
+++ src/helpers/connectorConfig/FormField.tsx
@@ -36,12 +36,11 @@ export const FormField: React.FC<FormFieldProps> = ({ field, parentValue: propPa
   // For child fields (oneOf subfields), watch the parent field to get the current selected value
   let parentValue = propParentValue;
 
-  // Always call useWatch at the top level
+  // Always call useWatch at the top level - hooks must be called unconditionally
   const parentFieldPath = field.path.slice(0, -1).join('.');
   const parentFieldValue = useWatch({
     control,
     name: parentFieldPath,
-    disabled: !parentFieldPath || field.parentValue === undefined || !!propParentValue,
   });
 
   if (field.parentValue !== undefined && !propParentValue) {
