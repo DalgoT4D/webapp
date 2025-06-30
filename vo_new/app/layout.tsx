5ab30eee1b983316b8ad548f6ddf13828ff32ccd
himanshudube97 <himanshu.dube13@gmail.com>
2025-07-01 00:12:14 +0530

    zustand and swr setup done

--- vo_new/app/layout.tsx
+++ vo_new/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SWRProvider } from "@/lib/swr";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SWRProvider>
+          {children}
+        </SWRProvider>
       </body>
     </html>
   );
