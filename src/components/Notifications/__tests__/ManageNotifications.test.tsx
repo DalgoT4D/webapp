26b6adde193a70b2eabf2205dd7d42a901a37c7b
MohitAgrawal16 <mohitagrawal1684@gmail.com>
2025-08-14 15:43:35 +0530

    Fix import

--- src/components/Notifications/__tests__/ManageNotifications.test.tsx
+++ src/components/Notifications/__tests__/ManageNotifications.test.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { render, screen, fireEvent, within } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ManageNotifications from '../ManageNotifications';
 import useSWR from 'swr';
 
